36abadead896aac839d3fa1b717059a8df7df2d0
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-24 17:17:54 -0400

    created pdfClasses.php and updated pdfHomeStudents.php

--- omar/foro/profesor/pdf/pdfClasses.php
+++ omar/foro/profesor/pdf/pdfClasses.php
@@ -1,102 +1,56 @@
-<?php  
-require '../session.php';
-$ID = $_SESSION['id'];
-$re = mysql_query("SELECT year FROM colegio WHERE usuario='administrador'",$con);
-$re = mysql_fetch_assoc($re);
-$year = $re['year'];
-$cursos = $_POST['imprimir'];
-require('../../../fpdf16/fpdf.php');
+<?php
+require_once '../../../app.php';
 
-class PDF 
-{
-	
-	public $profesor = '';
-	public $curso = '';
-	function setProfesor($profesor){
-	 	$this->profesor = $profesor;
-	 }
-	function setCurso($curso){
-	 	$this->curso = $curso;
-	 }
-//Cabecera de pagina
-	
-	function Header()
-	{	
-		require('../../control.php');
-	   
-		
-	    $tab = mysql_query("SELECT * FROM colegio WHERE usuario = 'administrador'", $con);	   
-		$colegio = mysql_fetch_object($tab);
-		$year = $colegio->year;
-		//informacion del colegio
-//		if($colegio->logo != "NO") $this->Image('../../logo/'.$colegio->logo,10,10,25);
-	    $this->Image('../../logo/logo.gif',10,10,25);
-		$this->SetFont('Arial','B',15);	
-		$this->Cell(0,5,$colegio->colegio,0,1,'C');
-		$this->SetFont('Arial','',9);	
-		$this->Cell(0,3,$colegio->dir1,0,1,'C');	
-		$this->Cell(0,5,$colegio->dir2,0,1,'C');	
-		$this->Cell(0,3,$colegio->pueblo1.', '.$colegio->esta1.' '.$colegio->zip1,0,1,'C');	
-		$this->SetFont('Arial','',8);
-		$this->Cell(0,3,'Tel. '.$colegio->telefono.' Fax '.$colegio->fax,0,1,'C');
-		$this->Cell(0,3,$colegio->correo,0,1,'C');
-		$this->Ln(10);			  	
-		$this->SetFont('Arial','',9);			
-		$this->Cell(95,5,$this->curso,0);
-		$this->Cell(95,5,$this->profesor,0,1,"R");
-		//columnas
-		$this->SetFont('Arial','B',13);
-		$this->SetFillColor(195,217,255);				
-		$this->Cell(10,7,"","LTB",0,"C",true);	
-		$this->Cell(15,7,"ID","RTB",0,"C",true);	
-		$this->Cell(50,7,"Apellido",1,0,"C",true);
-		$this->Cell(40,7,"Nombre",1,0,"C",true);			
-		$this->Cell(75,7,"Firma",1,0,"C",true);							
-		$this->Ln();	
+use Classes\Controllers\Student;
+use Classes\Controllers\Teacher;
+use Classes\DataBase\DB;
+use Classes\PDF;
+use Classes\Server;
+use Classes\Session;
 
+Server::is_post();
 
-	}
-	function Footer()
-	{
-		
-		$footer = 'Pagina '.$this->PageNo().' de {nb} '.' | '.date("m-d-Y");		
-	    $this->SetY(-15);
-	    //Arial italic 8
-	    $this->SetFont('Arial','I',8);
-	    //Numero de pagina
-	    $this->Cell(0,10,$footer,0,0,'C');
-
-	}
- 
-}
+$classes = $_POST['class'];
 
 $pdf = new PDF();
-$pdf->AliasNbPages();
-foreach ($cursos as $curso) {
+$pdf->SetTitle('Lista de estudiantes');
+$pdf->Fill();
+
+$teacher = new Teacher(Session::id());
+
+foreach ($classes as $class) {
+	$year = $teacher->info('year');
+	$thisClass = DB::table('cursos')
+		->where([
+			['year', $year],
+			['curso', $class]
+		])->first();
+
+	$pdf->AddPage();
+	$pdf->SetFont('Arial', 'B', 12);
+	$pdf->Cell(95, 5, "$class - $thisClass->desc1", 0);
+	$pdf->Cell(0, 5, $teacher->fullName(), 0, 1, "R");
+	// table header
+	$pdf->Cell(10, 7, "", "LTB", 0, "C", true);
+	$pdf->Cell(15, 7, "ID", "RTB", 0, "C", true);
+	$pdf->Cell(50, 7, "Apellidos", 1, 0, "C", true);
+	$pdf->Cell(40, 7, "Nombre", 1, 0, "C", true);
+	$pdf->Cell(75, 7, "Firma", 1, 1, "C", true);
+
+	$students = new Student();
+	$students = $students->findByClass($class);
+
 	$num = 1;
-	$re = mysql_query("SELECT p.nombre,p.apellidos,c.desc1 FROM profesor as p
-	INNER JOIN cursos as c on c.id = p.id WHERE  c.year='$year' AND c.curso='$curso'",$con);
-	$re = mysql_fetch_object($re);	
-	$nombreProfesor = $re->nombre." ".$re->apellidos;
-	$pdf->setCurso(utf8_decode("Curso $curso - $re->desc1"));	
-	$pdf->setProfesor($nombreProfesor);
-	$pdf->AddPage();	
-	$result = mysql_query("SELECT e.nombre,e.apellidos,e.id FROM padres as p 
-	INNER JOIN year AS e ON p.ss = e.ss WHERE e.year='$year' AND p.curso='$curso' AND p.year='$year' AND p.id ='$ID' and baja='' ORDER BY e.apellidos",$con);
-	while ($estudiante = mysql_fetch_object($result)) {		
-		$pdf->SetFont('Arial','',10);
-		$pdf->SetFillColor(229, 236, 249);		
-		$pdf->Cell(10,5,$num,0,0,"R");	
-		$pdf->Cell(15,5,$estudiante->id,0,0,"C");	
-		$pdf->Cell(50,5,ucwords(utf8_decode($estudiante->apellidos)));
-		$pdf->Cell(40,5,ucwords(utf8_decode($estudiante->nombre)));		
-		$pdf->Cell(75,5,"","B",0,"C");				
-		$pdf->Ln();	
+	$pdf->SetFont('Arial', '', 10);
+	foreach ($students as $student) {
+		$pdf->Cell(10, 5, $num, 0, 0, "R");
+		$pdf->Cell(15, 5, $student->id, 0, 0, "C");
+		$pdf->Cell(50, 5, ucwords(utf8_decode($student->apellidos)));
+		$pdf->Cell(40, 5, ucwords(utf8_decode($student->nombre)));
+		$pdf->Cell(75, 5, "", "B", 0, "C");
+		$pdf->Ln();
 		$num++;
 	}
-		
 }
 
 $pdf->Output();
-
-?>
--- omar/foro/profesor/pdf/pdfHomeStudents.php
+++ omar/foro/profesor/pdf/pdfHomeStudents.php
@@ -9,43 +9,42 @@ use Classes\Controllers\Teacher;
 
 $teacher = new Teacher(Session::id());
 $students = DB::table('year')
-   ->where([
-      ['grado', $teacher->grado],
-      ['year', $teacher->info('year')],
-      ['fecha_baja', '0000-00-00']
-   ])->orderBy('apellidos')->get();
+	->where([
+		['grado', $teacher->grado],
+		['year', $teacher->info('year')],
+		['fecha_baja', '0000-00-00']
+	])->orderBy('apellidos')->get();
 
 $pdf = new PDF();
 $pdf->AddPage();
-$pdf->SetTitle('Lista de clases');
+$pdf->SetTitle('Salon hogar');
 $pdf->Fill();
-$pdf->SetFont('Arial');
+
+
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(50, 5, 'Salon Hogar: ' . $teacher->grado, 0, 0, "L");
+$pdf->Cell(0, 5, $teacher->fullName(), 0, 1, "R");
+// table header
+$pdf->SetFont('Arial', 'B', 13);
+$pdf->Cell(10, 7, "", "LTB", 0, "C", true);
+$pdf->Cell(15, 7, "ID", "RTB", 0, "C", true);
+$pdf->Cell(60, 7, "Apellidos", 1, 0, "C", true);
+$pdf->Cell(40, 7, "Nombre", 1, 0, "C", true);
+$pdf->Cell(35, 7, "Usuario", 1, 0, "C", true);
+$pdf->Cell(35, 7, "Clave", 1, 1, "C", true);
+
+$pdf->SetFont('Arial', '', 10);
 
 $num = 1;
+foreach ($students as $student) {
 
-$pdf->SetFont('Arial','',10);
-$pdf->Cell(50,5,'Salon Hogar: ' . $teacher->grado,0,0,"L");
-$pdf->Cell(0,5,$teacher->fullName(),0,1,"R");
-
-$pdf->SetFont('Arial','B',13);
-$pdf->Cell(10,7,"","LTB",0,"C",true);	
-$pdf->Cell(15,7,"ID","RTB",0,"C",true);	
-$pdf->Cell(60,7,"Apellido",1,0,"C",true);
-$pdf->Cell(40,7,"Nombre",1,0,"C",true);			
-$pdf->Cell(35,7,"Usuario",1,0,"C",true);							
-$pdf->Cell(35,7,"Clave",1,1,"C",true);	
-
-$pdf->SetFont('Arial','',10);
-
-foreach ($students as $student) {	
-
-	$pdf->Cell(10,5,$num,1,0,"R");	
-	$pdf->Cell(15,5,$student->id,1,0,"C");	
-	$pdf->Cell(60,5,ucwords(utf8_decode($student->apellidos)),1);
-	$pdf->Cell(40,5,ucwords(utf8_decode($student->nombre)),1);		
-	$pdf->Cell(35,5,$student->usuario,1,0,"C");				
-	$pdf->Cell(35,5,$student->clave,1,0,"C");				
-	$pdf->Ln();	
+	$pdf->Cell(10, 5, $num, 1, 0, "R");
+	$pdf->Cell(15, 5, $student->id, 1, 0, "C");
+	$pdf->Cell(60, 5, ucwords(utf8_decode($student->apellidos)), 1);
+	$pdf->Cell(40, 5, ucwords(utf8_decode($student->nombre)), 1);
+	$pdf->Cell(35, 5, $student->usuario, 1, 0, "C");
+	$pdf->Cell(35, 5, $student->clave, 1, 0, "C");
+	$pdf->Ln();
 	$num++;
 }
 
