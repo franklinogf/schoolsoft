a7959d427c4326f12eea584dc9de4d8158bdf685
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-24 13:08:43 -0400

    orWhere working

--- Classes/DataBase/DB.php
+++ Classes/DataBase/DB.php
@@ -15,9 +15,13 @@ class DB extends DataBase
   private static $query = '';
   private static $orderBy = null;
   private static $findby = null;
+  private static $where = [];
   private static $whereCols = [];
   private static $whereValues = [];
   private static $whereOperators = [];
+  private static $orWhereCols = [];
+  private static $orWhereValues = [];
+  private static $orWhereOperators = [];
   private static $innerJoinTable = [];
   private static $innerJoinCol1 = [];
   private static $innerJoinCol2 = [];
@@ -41,7 +45,7 @@ class DB extends DataBase
     return self::$instance;
   }
 
-  public function orderBy($col, $mode = 'DESC')
+  public function orderBy($col, $mode = null)
   {
 
     self::$orderBy = ' ORDER BY ' . trim($col) . ' ' . trim($mode);
@@ -56,7 +60,6 @@ class DB extends DataBase
     $obj = $this->selectOne(self::$query, [trim($value)]);
     return $obj;
   }
-
 
   public function where($w1, $w2 = false, $w3 = false)
   {
@@ -77,6 +80,31 @@ class DB extends DataBase
         self::$whereOperators[] = (isset($w1[2]) ? trim($w1[1]) : '=');
       }
     }
+
+    return self::$instance;
+  }
+
+  public function orWhere($w1, $w2 = false, $w3 = false)
+  {
+    if ($w2) {
+      self::$orWhereCols[] = trim($w1);
+      self::$orWhereValues[] = ($w3 ? trim($w3) : trim($w2));
+      self::$orWhereOperators[] = ($w3 ? trim($w2) : '=');
+    } else if (!$w2) {
+      if ($this->isMultiArray($w1)) {
+        foreach ($w1 as $w) {
+          self::$orWhereCols[] = trim($w[0]);
+          self::$orWhereValues[] = (isset($w[2]) ? trim($w[2]) : trim($w[1]));
+          self::$orWhereOperators[] = (isset($w[2]) ? trim($w[1]) : '=');
+        }
+      } else {
+        self::$orWhereCols[] = trim($w1[0]);
+        self::$orWhereValues[] = (isset($w1[2]) ? trim($w1[2]) : trim($w1[1]));
+        self::$orWhereOperators[] = (isset($w1[2]) ? trim($w1[1]) : '=');
+      }
+    }
+
+
     return self::$instance;
   }
 
@@ -92,7 +120,7 @@ class DB extends DataBase
   public function get()
   {
     $this->buildSelectQuery();
-    $obj = $this->selectAll(self::$query, self::$whereValues);   
+    $obj = $this->selectAll(self::$query, self::$where);
     $this->closeDB();
     return $obj;
   }
@@ -100,34 +128,40 @@ class DB extends DataBase
   public function first()
   {
     $this->buildSelectQuery('limit 1');
-    $obj = $this->selectOne(self::$query, self::$whereValues);
+    $obj = $this->selectOne(self::$query, self::$where);
     $this->closeDB();
     return $obj;
   }
 
   private function buildSelectQuery($other = '')
   {
+    // for the limit or other
     $other = trim($other);
 
-    $where = '';    
-    $count = 0;
+    $where = '';
     if (count(self::$whereCols) > 0) {
       foreach (self::$whereCols  as $i => $col) {
-        $where .= ($count > 0 ? ' AND' : ' WHERE');
+        $where .= ($i > 0 ? ' AND' : ' WHERE');
         $where .= ' ' . $col . ' ' . self::$whereOperators[$i] . ' ?';
-        $count++;
       }
     }
 
-    $join = '';    
-    if(count(self::$innerJoinTable) > 0){
-      foreach (self::$innerJoinTable  as $i => $table) {       
-        $join .= ' INNER JOIN ' . $table . ' ON ' . self::$innerJoinCol1[$i] .' '. self::$innerJoinOperator[$i] . ' ' . self::$innerJoinCol2[$i];        
+    if (count(self::$orWhereCols) > 0) {
+      foreach (self::$orWhereCols  as $i => $col) {
+        $where .= ' OR ' . $col . ' ' . self::$orWhereOperators[$i] . ' ?';
       }
     }
 
-    echo self::$query = 'SELECT ' . self::$columns . ' FROM ' . self::$table . $join . $where . self::$orderBy . ' ' . $other;
-  
+    self::$where = array_merge(self::$whereValues, self::$orWhereValues);
+
+    $join = '';
+    if (count(self::$innerJoinTable) > 0) {
+      foreach (self::$innerJoinTable  as $i => $table) {
+        $join .= ' INNER JOIN ' . $table . ' ON ' . self::$innerJoinCol1[$i] . ' ' . self::$innerJoinOperator[$i] . ' ' . self::$innerJoinCol2[$i];
+      }
+    }
+
+    self::$query = 'SELECT ' . self::$columns . ' FROM ' . self::$table . $join . $where . self::$orderBy . ' ' . $other;
   }
 
   private function isMultiArray($array)
@@ -137,20 +171,24 @@ class DB extends DataBase
     return false;
   }
 
-  private function closeDB(){
+  private function closeDB()
+  {
     self::$instance = null;
     self::$table = '';
     self::$columns = '*';
     self::$query = '';
     self::$orderBy = null;
     self::$findby = null;
+    self::$where = [];
     self::$whereCols = [];
     self::$whereValues = [];
     self::$whereOperators = [];
+    self::$orWhereCols = [];
+    self::$orWhereValues = [];
+    self::$orWhereOperators = [];
     self::$innerJoinTable = [];
     self::$innerJoinCol1 = [];
     self::$innerJoinCol2 = [];
     self::$innerJoinOperator = [];
-
   }
 }
