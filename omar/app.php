1e535d42af712877589ed1ff089a0c032afaf938
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-24 14:57:16 -0400

    added pdf fill color constants

--- omar/app.php
+++ omar/app.php
@@ -16,30 +16,42 @@ define('__DB_NAME', "demo");
 
 /* ------------------------------ don't change ------------------------------ */
 
-define('__ROOT_SCHOOL',str_replace('/',DIRECTORY_SEPARATOR,__DIR__));
-define('__ROOT', str_replace('/',DIRECTORY_SEPARATOR,dirname(__DIR__)));
+define('__ROOT_SCHOOL', str_replace('/', DIRECTORY_SEPARATOR, __DIR__));
+define('__ROOT', str_replace('/', DIRECTORY_SEPARATOR, dirname(__DIR__)));
 define('__ROOT_URL', substr($_SERVER['PHP_SELF'], 0, - (strlen($_SERVER['SCRIPT_FILENAME']) - strlen(__ROOT_SCHOOL))));
-$root = str_replace(__ROOT_SCHOOL,'', str_replace('/',DIRECTORY_SEPARATOR,$_SERVER['SCRIPT_FILENAME'])); 
-define('__SUB_ROOT_URL', str_replace('\\','/',substr($root,0,strpos($root,DIRECTORY_SEPARATOR,1))));
+$root = str_replace(__ROOT_SCHOOL, '', str_replace('/', DIRECTORY_SEPARATOR, $_SERVER['SCRIPT_FILENAME']));
+define('__SUB_ROOT_URL', str_replace('\\', '/', substr($root, 0, strpos($root, DIRECTORY_SEPARATOR, 1))));
 
 
 /* -------------------------------------------------------------------------- */
 /*                              Files Directories                             */
 /* -------------------------------------------------------------------------- */
 
-define('__TEACHER_PROFILE_PICTURE', str_replace('/',DIRECTORY_SEPARATOR,__ROOT_URL.'/pictures/teachers/'));
-define('__STUDENT_PROFILE_PICTURE', str_replace('/',DIRECTORY_SEPARATOR,__ROOT_URL.'/pictures/students/'));
-define('__NO_PROFILE_PICTURE', str_replace('/',DIRECTORY_SEPARATOR,'/images/none.jpg'));
-define('__LOGO_PATH', str_replace('/',DIRECTORY_SEPARATOR,__ROOT_URL.'/logo/'));
+define('__TEACHER_PROFILE_PICTURE', str_replace('/', DIRECTORY_SEPARATOR, __ROOT_URL . '/pictures/teachers/'));
+define('__STUDENT_PROFILE_PICTURE', str_replace('/', DIRECTORY_SEPARATOR, __ROOT_URL . '/pictures/students/'));
+define('__NO_PROFILE_PICTURE', str_replace('/', DIRECTORY_SEPARATOR, '/images/none.jpg'));
+define('__LOGO_PATH', str_replace('/', DIRECTORY_SEPARATOR, __ROOT_URL . '/logo/'));
 
 
 /* -------------------------------------------------------------------------- */
 /*                                   Config                                   */
 /* -------------------------------------------------------------------------- */
 
-// lenguaje a usar
+// Language: es = Spanish, en = English
 define('__LANG', 'es');
 define('__DEFAULT_LOGO', 'logo.gif');
+
+/* ----------------------------------- PDF ---------------------------------- */
+// RGB $pdf->SetFillColor(red,green,blue)
+define('__PDF_FILL_COLOR', [
+   'red' => 229,
+   'green' => 236,
+   'blue' => 249
+]);
+
+
+
+/* -------------------------------- time zone ------------------------------- */
 
 /** For the United States:
  * Eastern ........... America/New_York
