525bf34d6e2d29fd0b3da3abd254881977596817
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-24 17:10:59 -0400

    updated with DB class

--- Classes/Models/StudentModel.php
+++ Classes/Models/StudentModel.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Classes\Models;
 
 use Classes\Controllers\School;
@@ -16,58 +17,79 @@ class StudentModel extends School
 
   protected function getStudentByPK($pk)
   {
-    $query = "SELECT * FROM {$this->table} WHERE {$this->primary_key} = ?";
-    
-    return $this->selectOne($query,[$pk]);
+    $obj = parent::table($this->table)
+      ->where($this->primary_key, $pk)->first();
+
+    return $obj;
   }
 
   protected function getStudentBySS($ss)
   {
 
-    $query = "SELECT * FROM {$this->table} WHERE `ss` = ? AND `year` = ? ";
     $year = $this->info('year');
-
-    return $this->selectOne($query,[$ss,$year]);
+    $obj = parent::table($this->table)->where([
+      ['year', $year],
+      ['ss', $ss]
+    ])->first();
+    return $obj;
   }
 
   protected function getAllStudents()
   {
-    $query = "SELECT * FROM {$this->table} WHERE  `year` = ? AND `fecha_baja`='0000-00-00' ORDER BY `apellidos`";
     $year = $this->info('year');
-    return $this->selectAll($query,[$year]);
+    $obj = parent::table($this->table)->where([
+      ['year', $year],
+      ['baja', '']
+    ])->orderBy('apellidos')->get();
+    return $obj;
   }
 
   protected function getStudentClasses($ss)
   {
-    $query = "SELECT DISTINCT `curso`, `descripcion` FROM `padres` WHERE  `year` = ? and `ss` = ? ORDER BY `curso`";
     $year = $this->info('year');
-    return $this->selectAll($query,[$year,$ss]);
-
+    $obj = parent::table('padres')
+      ->select('DISTINCT curso, descripcion')
+      ->where([
+        ['year', $year],
+        ['ss', $ss]
+      ])->orderBy('curso')->get();
+    return $obj;
   }
   protected function getStudentByUser($username)
   {
-    $query = "SELECT * FROM {$this->table} WHERE `usuario`= ? ";
-    return $this->selectOne($query,[$username]);
+    $obj = parent::table($this->table)->where('usuario', $username)->first();
+
+    return $obj;
   }
+
   protected function getStudentsByClass($class)
   {
-    $query = "SELECT `e`.* FROM `padres` as `p` 
-    INNER JOIN `year` AS `e` ON `p`.`ss` = `e`.`ss` 
-    WHERE `p`.`curso` = ? AND `e`.`year` = ? AND `p`.`year` = ? and `e`.`fecha_baja`='0000-00-00'
-    ORDER BY `e`.`apellidos`";
-    
     $year = $this->info('year');
-    return $this->selectAll($query,[$class,$year,$year]);
+
+    $obj = parent::table('padres')
+      ->select('year.*')
+      ->join('year', 'padres.ss', '=', 'year.ss')
+      ->where([
+        ['padres.curso', $class],
+        ['year.year', $year],
+        ['padres.year', $year],
+        ['baja', '']
+      ])->orderBy('year.apellidos')->get();
+
+    return $obj;
   }
 
   protected function studentLogin($username, $password)
   {
-    $query = "SELECT * FROM {$this->table} WHERE usuario= ? AND clave = ?";
-    return $this->selectOne($query,[$username,$password]);
+    $obj = parent::table($this->table)->where([
+      ['usuario', $username],
+      ['clave', $password]
+    ])->first();
+    return $obj;
   }
 
   protected function updateStudent($propsArray)
   {
-    $this->updateTable($this->table,$this->primary_key,$this->{$this->primary_key},$propsArray);
+    $this->updateTable($this->table, $this->primary_key, $this->{$this->primary_key}, $propsArray);
   }
 }
