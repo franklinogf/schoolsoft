ffdae649e2fbe6e99e844806d6c3249a3d544d92
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-28 13:41:30 -0400

    changed to use session::id() instead of the Teacher Class

--- omar/foro/profesor/includes/comments.php
+++ omar/foro/profesor/includes/comments.php
@@ -3,7 +3,6 @@ require_once '../../../app.php';
 
 use Classes\Route;
 use Classes\Controllers\Topic;
-use Classes\Controllers\Teacher;
 use Classes\Server;
 use Classes\Session;
 
@@ -12,14 +11,12 @@ use Classes\Session;
 Server::is_post();
 
 
-$teacher = new Teacher(Session::id());
-
 $id_topic = $_POST['id_topic'];
 $comment = $_POST['comment'];
 
 $topic = new Topic($id_topic);
 
 
-$topic->newComment($teacher->id, $comment, 'p');
+$topic->newComment(Session::id(), $comment, 'p');
 
 Route::redirect('/profesor/viewTopic.php?id=' . $id_topic);
