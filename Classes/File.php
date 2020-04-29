22212497d625f4500432482da3b7a8aefab59b82
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-29 19:07:40 -0400

    remade the file class to be used in a better way

--- Classes/File.php
+++ Classes/File.php
@@ -2,56 +2,48 @@
 
 namespace Classes;
 
+use Classes\Util;
 
 class File
 {
-   public $file;
+   public $amount = 0;
+   public $files = [];
 
    public function __construct($file)
    {
 
-      if (isset($_FILES[$file])) {
-         $this->file = $_FILES[$file];
+      if (isset($_FILES[$file]) && !empty($file)) {
+
+         foreach ($_FILES[$file]['name'] as $i => $thisFile) {
+            $this->files[$i]['name'] =  $_FILES[$file]['name'][$i];
+            $this->files[$i]['tmp'] =  $_FILES[$file]['tmp_name'][$i];
+            $this->files[$i]['size'] =  $_FILES[$file]['size'][$i];
+            $this->files[$i]['type'] =  $_FILES[$file]['type'][$i];
+            $this->files[$i]['error'] =  $_FILES[$file]['error'][$i];
+         }
+         $this->files = Util::toObject($this->files);
+
+         $this->amount = count($this->files);
          return true;
       } else {
          return false;
       }
    }
 
-   public  function upload($name, $path)
-   {
+   public static function upload($file, $path, $name = false)
+   {      
+      $newName = (!$name) ? $file->name : $name;
+      $fullPath = __ROOT_SCHOOL . DIRECTORY_SEPARATOR . str_replace('/',DIRECTORY_SEPARATOR,$path);
 
-      $fullPath = __ROOT . DIRECTORY_SEPARATOR . $path;
       if (!is_dir($fullPath)) {
          mkdir($fullPath, 0777, true);
       }
-     if( move_uploaded_file($this->tmp(), $fullPath . $name)){
-        return true;
-     }else{
-        return false;
-     }
-   }
-
-   public function name()
-   {
-      return $this->file['name'];
-   }
-
-   public function tmp()
-   {
-      return $this->file['tmp_name'];
-   }
-
-   public function size()
-   {
-      return $this->file['size'];
-   }
-   public function type()
-   {
-      return $this->file['type'];
-   }
-   public function error()
-   {
-      return $this->file['error'];
+      
+      $filePath = $fullPath.$newName;
+      if (move_uploaded_file($file->tmp,$filePath )) {
+         return true;
+      } else {
+         return false;
+      }
    }
 }
