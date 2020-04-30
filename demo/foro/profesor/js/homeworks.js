a8c83b916812bc76ed7b18dff48ec3860cbca19d
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-29 23:25:49 -0400

    Fixed a bug where the tooltip did not want to hide when the fontawasome icon was clicked to delete

--- demo/foro/profesor/js/homeworks.js
+++ demo/foro/profesor/js/homeworks.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
-      
-$(".delHomework").click((e) =>{
-  if(confirm("¿Esta seguro de que desea borrar esta tarea?")){
-   $(e.target).parents('.homework').hide('drop',{ direction: "down" },500,()=>{
-      $(e.target).tooltip('hide')
-      $(e.target).parents('.homework').remove();
-   });
-  }
-})
+
+  $(".delHomework").click((e) => {
+    if (confirm("¿Esta seguro de que desea borrar esta tarea?")) {
+      $(e.target).parents('.homework').hide('drop', { direction: "down" }, 'slow', () => {        
+        if(e.target.tagName === 'I'){
+          $(e.target).parent().tooltip('hide')
+        }else{
+          $(e.target).tooltip('hide')
+        }
+        $(e.target).parents('.homework').remove();
+      });
+    }
+  })
 
 
 
