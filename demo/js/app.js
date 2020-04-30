6ada1df0eb0c09d0195c176a979b6c5e6b491a1a
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-30 13:06:10 -0400

    added a function to make animations with animate.css and stop using jqueryUI

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,5 +1,42 @@
 //* --------------------------- functions --------------------------- *//
 
+// function for animations
+/** 
+Animations class
+bounce         	flash           	pulse            	rubberBand
+shake          	headShake       	swing            	tada
+wobble         	jello           	bounceIn         	bounceInDown
+bounceInLeft   	bounceInRight   	bounceInUp       	bounceOut
+bounceOutDown  	bounceOutLeft   	bounceOutRight   	bounceOutUp
+fadeIn         	fadeInDown      	fadeInDownBig    	fadeInLeft
+fadeInLeftBig  	fadeInRight     	fadeInRightBig   	fadeInUp
+fadeInUpBig    	fadeOut         	fadeOutDown      	fadeOutDownBig
+fadeOutLeft    	fadeOutLeftBig  	fadeOutRight     	fadeOutRightBig
+fadeOutUp      	fadeOutUpBig    	flipInX          	flipInY
+flipOutX       	flipOutY        	lightSpeedIn     	lightSpeedOut
+rotateIn       	rotateInDownLeft	rotateInDownRight	rotateInUpLeft
+rotateInUpRight	rotateOut       	rotateOutDownLeft	rotateOutDownRight
+rotateOutUpLeft	rotateOutUpRight	hinge            	jackInTheBox
+rollIn         	rollOut         	zoomIn           	zoomInDown
+zoomInLeft     	zoomInRight     	zoomInUp         	zoomOut
+zoomOutDown    	zoomOutLeft     	zoomOutRight     	zoomOutUp
+slideInDown    	slideInLeft     	slideInRight     	slideInUp
+slideOutDown   	slideOutLeft    	slideOutRight    	slideOutUp
+heartBeat			*/ 
+function animateCSS(element, animationName, callback) {
+  const node = $(element)
+  node.addClass(`animated ${animationName}`)
+
+  function handleAnimationEnd() {
+      node.removeClass(`animated ${animationName}`)
+      node.off('animationend', handleAnimationEnd)
+
+      if (typeof callback === 'function') callback()
+  }
+
+  node.on('animationend', handleAnimationEnd)
+}
+// fomart date
 function formatDate(value) {
 
   if (value !== '0000-00-00') {
@@ -68,6 +105,7 @@ function checkPasswords(pass1 = '#pass1', pass2 = '#pass2', bothClass = '.pass')
   }
   return true;
 }
+
 
 $(function () {
   // Data table global configuration
@@ -155,5 +193,8 @@ $(function () {
 
   // enable tooltips 
   $('[data-toggle="tooltip"]').tooltip()
+
+
+  
 
 });
