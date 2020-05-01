4225b496bef408abab22395caaaf1796c5dc174d
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-30 23:05:32 -0400

    added fa icons to the pdf options

--- demo/foro/profesor/includes/layouts/menu.php
+++ demo/foro/profesor/includes/layouts/menu.php
@@ -35,8 +35,8 @@ global $teacher;
           <a class="dropdown-item  <?= ($pathFile === 'listClasses' ? 'active' : '') ?>" href="<?= Route::url('/foro/profesor/listClasses.php') ?>">Lista de cursos</a>
           <a class="dropdown-item" <?= ($pathFile === 'homeworksList' ? 'active' : '') ?>" href="<?= Route::url('/foro/profesor/homeworksList.php') ?>">Tareas Entregadas</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" target="_blank" href="<?= Route::url('/foro/profesor/pdf/pdfHomeStudents.php') ?>">Salon Hogar</a>
-          <a class="dropdown-item" target="_blank" href="<?= Route::url('/foro/profesor/pdf/pdfUsersList.php') ?>">Lista de Usuarios</a>
+          <a class="dropdown-item" target="_blank" href="<?= Route::url('/foro/profesor/pdf/pdfHomeStudents.php') ?>"><i class="far fa-file-pdf"></i> Salon Hogar</a>
+          <a class="dropdown-item" target="_blank" href="<?= Route::url('/foro/profesor/pdf/pdfUsersList.php') ?>"><i class="far fa-file-pdf"></i></i> Lista de Usuarios</a>
         </div>
       </li>
       <li class="nav-item dropdown">
