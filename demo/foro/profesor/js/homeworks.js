ff861da633526f9a187a431902918b93c19fab7b
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-01 19:50:56 -0400

    change $.ajax for $.post

--- demo/foro/profesor/js/homeworks.js
+++ demo/foro/profesor/js/homeworks.js
@@ -9,39 +9,35 @@ $(document).ready(function () {
       $(e.target).parent().tooltip('hide').data('homeworkId') :
       $(e.target).tooltip('hide').data('homeworkId');
 
-    $.ajax({
-      type: "POST",
-      url: includeThisFile(),
-      data: { 'getHomework': document_id },
-      dataType: 'json',
-      success: function (res) {
-        $("#homeworkFormBtn").prop('name', 'editHomework');
-        $("form").prepend(`<input type="hidden" name="document_id" id="homework_id" value="${document_id}"/>`)
-        $("#title").val(res.titulo)
-        $("#description").val(res.descripcion)
-        $("#class").val(res.curso)
-        $("#sinceDate").val(res.fec_in === '0000-00-00' ? '' : res.fec_in)
-        $("#untilDate").val(res.fec_out === '0000-00-00' ? '' : res.fec_out)
-        $("#radio1").prop('checked', res.enviartarea === 'si' ? true : false)
-        $("#radio2").prop('checked', res.enviartarea === 'si' ? false : true)
-        $("#link1").val(res.lin1)
-        $("#link2").val(res.lin2)
-        $("#link3").val(res.lin3)
-        if (res.archivos) {
-          res.archivos.forEach(file => {
-            addExistingFile(fileRealName(file.nombre), file.id)
-          });
-        }
-        // scroll the view to the form when the edit homework button is pressed
+    $.post(includeThisFile(), { 'getHomework': document_id }, res => {
 
-        $('html').animate({
-          scrollTop: $("form").offset().top
-        }, 500, () => {
-          animateCSS('form', 'pulse')
-
+      $("#homeworkFormBtn").prop('name', 'editHomework');
+      $("form").prepend(`<input type="hidden" name="document_id" id="homework_id" value="${document_id}"/>`)
+      $("#title").val(res.titulo)
+      $("#description").val(res.descripcion)
+      $("#class").val(res.curso)
+      $("#sinceDate").val(res.fec_in === '0000-00-00' ? '' : res.fec_in)
+      $("#untilDate").val(res.fec_out === '0000-00-00' ? '' : res.fec_out)
+      $("#radio1").prop('checked', res.enviartarea === 'si' ? true : false)
+      $("#radio2").prop('checked', res.enviartarea === 'si' ? false : true)
+      $("#link1").val(res.lin1)
+      $("#link2").val(res.lin2)
+      $("#link3").val(res.lin3)
+      if (res.archivos) {
+        res.archivos.forEach(file => {
+          addExistingFile(fileRealName(file.nombre), file.id)
         });
       }
-    });
+      // scroll the view to the form when the edit homework button is pressed
+      $('html').animate({
+        scrollTop: $("form").offset().top
+      }, 500, () => {
+        animateCSS('form', 'pulse')
+      });
+    },
+      "json"
+    );
+   
 
   })
 
