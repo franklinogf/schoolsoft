e60a5855aa211a5dc183c814222c46ebb5505aef
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-01 20:43:13 -0400

    added a empty value to the classes select input

--- demo/foro/profesor/homeworks.php
+++ demo/foro/profesor/homeworks.php
@@ -117,10 +117,10 @@ $classes = $teacher->classes();
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
 
          <?php foreach ($homeworks as $homework) : ?>
-         <?php $expired = ($homework->fec_out >= Util::date() || $homework->fec_out === '0000-00-00' ? 'text-success' : 'text-warning'); ?>
+            <?php $expired = ($homework->fec_out >= Util::date() || $homework->fec_out === '0000-00-00' ? 'text-success' : 'text-warning'); ?>
             <div class="col mb-4 homework <?= $homework->id_documento ?>">
                <div class="card">
-                  <h6 class="card-header bg-primary d-flex justify-content-between"><?= $homework->curso ?> <i class="fas fa-circle <?= $expired ?>"></i></h6> 
+                  <h6 class="card-header bg-primary d-flex justify-content-between"><?= $homework->curso ?> <i class="fas fa-circle <?= $expired ?>"></i></h6>
                   <div class="card-body ">
                      <h5 class="card-title"><?= $homework->titulo ?></h5>
                      <p class="card-text"><?= $homework->descripcion ?></p>
@@ -140,7 +140,7 @@ $classes = $teacher->classes();
                      <?php if (property_exists($homework, 'archivos')) : ?>
                         <div class="btn-group-vertical w-100 mt-2">
                            <?php foreach ($homework->archivos as $i => $file) : ?>
-                              <a data-file-id="<?= $file->id ?>" href="#" target="_blank" data-toggle="tooltip" title='<?= File::name($file->nombre,true) ?>' class="btn btn-outline-secondary btn-sm"><?= File::faIcon(File::extension($file->nombre)) . " Archivo " . ($i + 1) ?> </a>
+                              <a data-file-id="<?= $file->id ?>" href="#" target="_blank" data-toggle="tooltip" title='<?= File::name($file->nombre, true) ?>' class="btn btn-outline-secondary btn-sm"><?= File::faIcon(File::extension($file->nombre)) . " Archivo " . ($i + 1) ?> </a>
                            <?php endforeach ?>
                         </div>
                      <?php endif ?>
