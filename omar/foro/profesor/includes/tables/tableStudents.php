f8893f04bba680c026f7a725930fe18c1daa7a3a
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-05 16:13:41 -0400

    added an empty table for the students

---
+++ omar/foro/profesor/includes/tables/tableStudents.php
@@ -0,0 +1,21 @@
+<!-- Students table -->
+
+<table class="studentsTable table table-striped table-hover cell-border w-100 shadow">
+   <thead class="bg-gradient-primary bg-primary border-0">
+      <tr>
+         <th>Estudiante</th>
+         <th>Usuario</th>
+      </tr>
+   </thead>
+   <tbody>
+   </tbody>
+   <tfoot>
+      <tr class="bg-gradient-secondary bg-secondary">
+         <th>Estudiante</th>
+         <th>Usuario</th>
+      </tr>
+      <tr class="bg-gradient-light bg-light">
+         <td colspan="2"><button id="back" type="button" class="btn btn-block btn-primary">Atrás</button></td>
+      </tr>
+   </tfoot>
+</table>
