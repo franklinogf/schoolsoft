1da8057fa8aa94adff3c10cc97a38cf7a58781c3
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-05-06 16:00:35 -0400

    just add file when file name is not empty

--- Classes/File.php
+++ Classes/File.php
@@ -22,30 +22,30 @@ class File
       "pptx" => '<i class="far fa-file-powerpoint"></i>',
       "doc" => '<i class="far fa-file-word"></i>',
       "docx" => '<i class="far fa-file-word"></i>',
-      "zip" => '<i class="far fa-file-archive"></i>'     
+      "zip" => '<i class="far fa-file-archive"></i>'
    ];
    public static $fileIcon = '<i class="far fa-file-alt"></i>';
 
    public function __construct($file = 'file')
    {
-      
-      if (isset($_FILES[$file]) && !empty($file) && $_FILES[$file]['error'] === 0) {
-         if($this->isMultiArray($_FILES[$file])){
+
+      if (isset($_FILES[$file]) && !empty($file)) {
+         if ($this->isMultiArray($_FILES[$file])) {
             foreach ($_FILES[$file]['name'] as $i => $name) {
-               $this->files[$i]['name'] =  $_FILES[$file]['name'][$i];
-               $this->files[$i]['tmp_name'] =  $_FILES[$file]['tmp_name'][$i];
-               $this->files[$i]['size'] =  $_FILES[$file]['size'][$i];
-               $this->files[$i]['type'] =  $_FILES[$file]['type'][$i];
-               $this->files[$i]['error'] =  $_FILES[$file]['error'][$i];
+               if ($_FILES[$file]['name'][$i] !== '') {
+                  $this->files[$i]['name'] =  $_FILES[$file]['name'][$i];
+                  $this->files[$i]['tmp_name'] =  $_FILES[$file]['tmp_name'][$i];
+                  $this->files[$i]['size'] =  $_FILES[$file]['size'][$i];
+                  $this->files[$i]['type'] =  $_FILES[$file]['type'][$i];
+                  $this->files[$i]['error'] =  $_FILES[$file]['error'][$i];
+               }
             }
             $this->files = Util::toObject($this->files);
-   
+
             $this->amount = count($this->files);
-            
-         }else{
+         } else {
             $this->files = Util::toObject($_FILES[$file]);
             $this->amount = 1;
-            
          }
          return true;
       } else {
@@ -54,16 +54,16 @@ class File
    }
 
    public static function upload($file, $path, $name = false)
-   {      
+   {
       $newName = (!$name) ? $file->name : $name;
-      $fullPath = __ROOT_SCHOOL . DIRECTORY_SEPARATOR . str_replace('/',DIRECTORY_SEPARATOR,$path);
+      $fullPath = __ROOT_SCHOOL . DIRECTORY_SEPARATOR . str_replace('/', DIRECTORY_SEPARATOR, $path);
 
       if (!is_dir($fullPath)) {
          mkdir($fullPath, 0777, true);
       }
-      
-      $filePath = $fullPath.$newName;
-      if (move_uploaded_file($file->tmp_name,$filePath )) {
+
+      $filePath = $fullPath . $newName;
+      if (move_uploaded_file($file->tmp_name, $filePath)) {
          return true;
       } else {
          return false;
@@ -71,38 +71,41 @@ class File
    }
 
    public static function delete($path, $fileName)
-   {           
-      $fullPath = __ROOT_SCHOOL . DIRECTORY_SEPARATOR . str_replace('/',DIRECTORY_SEPARATOR,$path);
-      
-      $filePath = $fullPath.$fileName;
+   {
+      $fullPath = __ROOT_SCHOOL . DIRECTORY_SEPARATOR . str_replace('/', DIRECTORY_SEPARATOR, $path);
+
+      $filePath = $fullPath . $fileName;
 
       if (file_exists($filePath)) {
-         if(unlink($filePath)){
+         if (unlink($filePath)) {
             return true;
          }
          return false;
       }
-      
+
       return false;
    }
 
-   public static function extension($fileName){
+   public static function extension($fileName)
+   {
       $fileInfo = pathinfo($fileName);
       return strtolower($fileInfo['extension']);
    }
 
-   public static function name($fileName,$baseName = false){
+   public static function name($fileName, $baseName = false)
+   {
       $fileInfo = pathinfo($fileName);
       $fileName = $fileInfo['filename'];
-      if($baseName){
-         $fileName = substr($fileName,strpos($fileName,")")+1);
+      if ($baseName) {
+         $fileName = substr($fileName, strpos($fileName, ")") + 1);
       }
       return trim($fileName);
    }
 
-   public static function faIcon($extension){
+   public static function faIcon($extension)
+   {
       $faIcon = self::$fileIcon;
-      if(array_key_exists($extension,self::$faIcons)){
+      if (array_key_exists($extension, self::$faIcons)) {
          $faIcon = self::$faIcons[$extension];
       }
       return $faIcon;
@@ -110,9 +113,9 @@ class File
 
 
    private function isMultiArray($array)
-  {
-    $rv = array_filter($array, 'is_array');
-    if (count($rv) > 0) return true;
-    return false;
-  }
+   {
+      $rv = array_filter($array, 'is_array');
+      if (count($rv) > 0) return true;
+      return false;
+   }
 }
