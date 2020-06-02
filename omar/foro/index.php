ae0bccc8994fd47e75053e833d4b4ea93e57ad5e
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-06-02 14:08:22 -0400

    change the img to be fluid

--- omar/foro/index.php
+++ omar/foro/index.php
@@ -6,7 +6,7 @@ use Classes\Controllers\School;
 use Classes\Session;
 
 if (Session::is_logged(false)) {
-   Route::redirect('/'.Session::type());
+   Route::redirect('/' . Session::type());
 }
 
 ?>
@@ -17,21 +17,21 @@ if (Session::is_logged(false)) {
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <title>Foro - Iniciar Sesión</title>
-   <link rel="icon" href="<?= School::logo() ?>"/>
+   <link rel="icon" href="<?= School::logo() ?>" />
    <?php
    Route::css("/css/main-bootstrap.css");
-   Route::css("/css/main.css",true);
+   Route::css("/css/main.css", true);
    Route::css("/foro/css/login.css");
    ?>
 </head>
 
 <body class="text-center">
+   <img class="mb-4 img-fluid" src="<?= School::logo() ?>" alt="Logo" width="<?= __LOGIN_LOGO_SIZE_W ?>" height="<?= __LOGIN_LOGO_SIZE_H ?>">
    <form class="form-signin" method='POST' action="<?= Route::url('/foro/includes/login.php') ?>">
-      <img class="mb-4" src="<?= School::logo() ?>" alt="Logo" width="<?= __LOGIN_LOGO_SIZE_W ?>" height="<?= __LOGIN_LOGO_SIZE_H ?>">
       <h1 class="h3 mb-3 font-weight-normal ">Iniciar Sesión</h1>
       <?php if (Session::get('errorLogin')) : ?>
          <div class="alert alert-danger animated zoomIn" role="alert">
-            <strong>Error!</strong> <?= Session::get('errorLogin',true) ?>
+            <strong>Error!</strong> <?= Session::get('errorLogin', true) ?>
          </div>
       <?php endif ?>
       <label for="username" class="sr-only">Usuario:</label>
@@ -39,7 +39,7 @@ if (Session::is_logged(false)) {
          <div class="input-group-prepend">
             <div class="input-group-text"><i class="fas fa-user fa-sm"></i></div>
          </div>
-         <input type="text" id="username" name="username" class="form-control" placeholder="Usuario" autofocus>         
+         <input type="text" id="username" name="username" class="form-control" placeholder="Usuario" autofocus>
       </div>
 
       <label for="password" class="sr-only">Clave:</label>
@@ -49,16 +49,16 @@ if (Session::is_logged(false)) {
          </div>
          <input type="password" id="password" name="password" class="form-control mb-0" placeholder="Clave">
       </div>
-      
+
       <button class="btn btn-lg btn-primary btn-block mt-2" type="submit">Continuar</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
    </form>
    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<?php
+   <?php
 
- Route::js('/js/app.js',true);
- Route::js('/foro/js/login.js');
-?>
+   Route::js('/js/app.js', true);
+   Route::js('/foro/js/login.js');
+   ?>
 </body>
 
 </html>
