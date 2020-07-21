4a2571dc00966ed9dccbf92b615b6f14b0b237eb
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-07-21 10:48:37 -0400

    separate database info to another file

---
+++ omar/database.php
@@ -0,0 +1,15 @@
+<?php
+/* -------------------------------------------------------------------------- */
+/*                         @Class DataBase Information                        */
+/* -------------------------------------------------------------------------- */
+
+// define('__HOST', "localhost");
+// define('__USERNAME', "root");
+// define('__PASSWORD', "");
+// define('__DB_NAME', "demo");
+
+define('__HOST', "104.238.127.126");
+define('__USERNAME', "admin_democol");
+define('__PASSWORD', "Amc123456");
+define('__DB_NAME', "admin_democol");
+
