e937e7fb535d338e8c98c4cefb79af1ab40466be
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-08-24 12:57:39 -0400

    added local and comentted remote database

--- omar/database.php
+++ omar/database.php
@@ -3,7 +3,16 @@
 /*                         @Class DataBase Information                        */
 /* -------------------------------------------------------------------------- */
 
-define('__HOST', "schoolsoftpr.com");
-define('__USERNAME', "admin_democol");
-define('__PASSWORD', "Amc123456");
-define('__DB_NAME', "admin_democol");
+/* ----------------------------- LOCAL DATABASE ----------------------------- */
+
+define('__HOST', "localhost");
+define('__USERNAME', "root");
+define('__PASSWORD', "");
+define('__DB_NAME', "demo");
+
+/* ----------------------------- REMOTE DATABASE ---------------------------- */
+
+// define('__HOST', "schoolsoftpr.com");
+// define('__USERNAME', "admin_democol");
+// define('__PASSWORD', "Amc123456");
+// define('__DB_NAME', "admin_democol");
