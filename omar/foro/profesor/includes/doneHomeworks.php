852cd28f5620bb9196e68dee58d5c20f3de028c0
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-08-24 12:56:59 -0400

    upper case name and formatted code

--- omar/foro/profesor/includes/doneHomeworks.php
+++ omar/foro/profesor/includes/doneHomeworks.php
@@ -14,39 +14,39 @@ Server::is_post();
 if (isset($_POST['homeworksByClass'])) {
    $class = $_POST['homeworksByClass'];
    $teacher = new Teacher(Session::id());
-   if ($data = $teacher->homeworks($class,false)) {
+   if ($data = $teacher->homeworks($class, false)) {
       $array = [
          'response' => true,
-         'data' => $data         
+         'data' => $data
       ];
    } else {
       $array = ['response' => false];
    }
    echo Util::toJson($array);
-}else if (isset($_POST['doneHomeworksByHomeworkId'])){
+} else if (isset($_POST['doneHomeworksByHomeworkId'])) {
    $id_homework = $_POST['doneHomeworksByHomeworkId'];
    $hw = new Homework($id_homework);
    $data = [];
    $doneHws = $hw->doneHomeworks();
-   foreach ($doneHws as $key => $doneHw) {     
+   foreach ($doneHws as $key => $doneHw) {
       $student = new Student($doneHw->id_estudiante);
       $data[$key]['id'] = $doneHw->id;
-      $data[$key]['nombre'] = $student->fullName();
-      $data[$key]['nota'] = $doneHw->nota;           
-      $data[$key]['fecha'] = Util::formatDate($doneHw->fecha,true);
+      $data[$key]['nombre'] = strtoupper($student->fullName());
+      $data[$key]['nota'] = $doneHw->nota;
+      $data[$key]['fecha'] = Util::formatDate($doneHw->fecha, true);
       $data[$key]['hora'] = Util::formatTime($doneHw->hora);
-      if(property_exists($doneHw,'archivos')){
+      if (property_exists($doneHw, 'archivos')) {
          foreach ($doneHw->archivos as $i => $file) {
             $data[$key]['archivos'][$i]['nombre'] = File::name($file->nombre, true);
-            $data[$key]['archivos'][$i]['url'] = __STUDENT_HOMEWORKS_DIRECTORY_URL.$file->nombre;
-            $data[$key]['archivos'][$i]['icon'] = File::faIcon(File::extension($file->nombre),'lg');
+            $data[$key]['archivos'][$i]['url'] = __STUDENT_HOMEWORKS_DIRECTORY_URL . $file->nombre;
+            $data[$key]['archivos'][$i]['icon'] = File::faIcon(File::extension($file->nombre), 'lg');
          }
       }
    }
    if ($data) {
       $array = [
          'response' => true,
-         'data' => $data         
+         'data' => $data
       ];
    } else {
       $array = ['response' => false];
