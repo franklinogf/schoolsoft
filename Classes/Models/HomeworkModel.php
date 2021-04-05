ccf7df7790a8f8a0237e6435f9c23017aad39ceb
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-04-05 12:48:23 -0400

    formatted

--- Classes/Models/HomeworkModel.php
+++ Classes/Models/HomeworkModel.php
@@ -18,7 +18,7 @@ class HomeworkModel extends School
 
    protected function getHomeworkByPK($pk)
    {
-      $obj =  parent::table($this->table,!__COSEY)
+      $obj =  parent::table($this->table, !__COSEY)
          ->where($this->primary_key, $pk)->first();
       $this->getFiles($obj);
       return $obj;
@@ -28,7 +28,7 @@ class HomeworkModel extends School
    protected function getAllHomeworks()
    {
 
-      $obj = parent::table($this->table,!__COSEY)
+      $obj = parent::table($this->table, !__COSEY)
          ->orderBy($this->primary_key, 'DESC')->get();
       $this->getFiles($obj);
       return $obj;
@@ -37,7 +37,7 @@ class HomeworkModel extends School
    protected function getDoneHomeworksByHomeworkId($id)
    {
       $hw = $this->getHomeworkByPK($id);
-      $doneHw = parent::table('tareas_enviadas',!__COSEY)->where([
+      $doneHw = parent::table('tareas_enviadas', !__COSEY)->where([
          ['id_tarea', $id],
          ['id_profesor', $hw->id2],
          ['year', $this->info('year')]
@@ -80,17 +80,16 @@ class HomeworkModel extends School
       if (!$all) {
          array_push($whereArray, ["{$this->table}.enviartarea", 'si']);
       }
-      if(__COSEY){
+      if (__COSEY) {
          array_push($whereArray, ["padres.year", $this->info('year')]);
-         $obj = parent::table($this->table,!__COSEY)->select("DISTINCT {$this->table}.*,padres.descripcion as `desc`")
-         ->join('padres', "padres.curso", "=", "{$this->table}.curso")
-         ->where($whereArray)->orderBy("{$this->table}.{$this->primary_key}", 'DESC')->get();
-         
-      }else{
+         $obj = parent::table($this->table, !__COSEY)->select("DISTINCT {$this->table}.*,padres.descripcion as `desc`")
+            ->join('padres', "padres.curso", "=", "{$this->table}.curso")
+            ->where($whereArray)->orderBy("{$this->table}.{$this->primary_key}", 'DESC')->get();
+      } else {
          array_push($whereArray, ["cursos.year", $this->info('year')]);
          $obj = parent::table($this->table)->select("{$this->table}.*,cursos.desc1 as `desc`")
-         ->join('cursos', "cursos.curso", "=", "{$this->table}.curso")
-         ->where($whereArray)->orderBy("{$this->table}.{$this->primary_key}", 'DESC')->get();
+            ->join('cursos', "cursos.curso", "=", "{$this->table}.curso")
+            ->where($whereArray)->orderBy("{$this->table}.{$this->primary_key}", 'DESC')->get();
       }
 
       $this->getFiles($obj);
@@ -100,26 +99,26 @@ class HomeworkModel extends School
    protected function getHomeworksByClassForStudents($class, $date = null)
    {
       $date = $date ? $date : Util::date();
-     if(__COSEY){
-      
-      $obj = parent::table($this->table,!__COSEY)->select("DISTINCT {$this->table}.*,padres.descripcion as `desc`")
-      ->join('padres', "padres.curso", "=", "{$this->table}.curso")
-      ->where([
-         ["{$this->table}.curso", $class],
-         // ["{$this->table}.enviartarea", 'si'],
-         ["padres.year", $this->info('year')]
-      ])->whereRaw("AND ({$this->table}.fec_out >= ? OR {$this->table}.fec_out = ?)", [$date, "0000-00-00"])
-      ->orderBy("{$this->table}.fec_out", 'DESC')->get();
-     }else{
-      $obj = parent::table($this->table,!__COSEY)->select("{$this->table}.*,cursos.desc1 as `desc`")
-      ->join('cursos', "cursos.curso", "=", "{$this->table}.curso")
-      ->where([
-         ["{$this->table}.curso", $class],
-         // ["{$this->table}.enviartarea", 'si'],
-         ["cursos.year", $this->info('year')]
-      ])->whereRaw("AND ({$this->table}.fec_out >= ? OR {$this->table}.fec_out = ?)", [$date, "0000-00-00"])
-      ->orderBy("{$this->table}.fec_out", 'DESC')->get();
-     }
+      if (__COSEY) {
+
+         $obj = parent::table($this->table, !__COSEY)->select("DISTINCT {$this->table}.*,padres.descripcion as `desc`")
+            ->join('padres', "padres.curso", "=", "{$this->table}.curso")
+            ->where([
+               ["{$this->table}.curso", $class],
+               // ["{$this->table}.enviartarea", 'si'],
+               ["padres.year", $this->info('year')]
+            ])->whereRaw("AND ({$this->table}.fec_out >= ? OR {$this->table}.fec_out = ?)", [$date, "0000-00-00"])
+            ->orderBy("{$this->table}.fec_out", 'ASC')->get();
+      } else {
+         $obj = parent::table($this->table, !__COSEY)->select("{$this->table}.*,cursos.desc1 as `desc`")
+            ->join('cursos', "cursos.curso", "=", "{$this->table}.curso")
+            ->where([
+               ["{$this->table}.curso", $class],
+               ["cursos.year", $this->info('year')],
+            ])
+            ->whereRaw("AND ({$this->table}.fec_out >=  ? OR {$this->table}.fec_out = ?)", [Util::date(), "0000-00-00"])
+            ->orderBy("{$this->table}.fec_out ASC")->get();
+      }
       $this->getFiles($obj);
       return $obj;
    }
@@ -132,7 +131,7 @@ class HomeworkModel extends School
          $obj = [$obj];
       }
       foreach ($obj as $homework) {
-         $files = parent::table($table,!__COSEY)
+         $files = parent::table($table, !__COSEY)
             ->where($whereCol, $homework->{$whereVal})->get();
          if ($files) {
             foreach ($files as $file) {
