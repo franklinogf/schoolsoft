65b4d810a45838180a975bff1bf69d5eb5fc55e8
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-04-06 11:17:06 -0400

    removed a bad ID prop

--- omar/foro/profesor/virtual.php
+++ omar/foro/profesor/virtual.php
@@ -39,7 +39,7 @@ $teacher = new Teacher(Session::id());
         <form class="needs-validation" novalidate>
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="virtualModal"></h5>
+              <h5 class="modal-title"></h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
