bed6dd6cf6f00cd324fb85bff477aa0564d6605e
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-05-25 10:30:38 -0400

    change size of the world gift

--- omar/regiweb/home.php
+++ omar/regiweb/home.php
@@ -24,7 +24,7 @@ $teacher = new Teacher(Session::id());
     ?>
     <div class="container-lg mt-lg-3  px-0">
         <h1 class="display-4 mt-5">Conectate desde cualquier parte del Mundo.</h1>
-        <img class="img-fluid mx-auto d-block mt-5 mt-lg-4" src="/images/globe.gif" />
+        <img class="img-fluid mx-auto d-block mt-5 mt-lg-4 w-25" src="/images/globe.gif" />
     </div>
     <?php
     Route::includeFile('/includes/layouts/scripts.php', true);
