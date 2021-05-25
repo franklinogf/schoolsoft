67341268882aed3adf0e16d7e7030add61f7fab5
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-05-25 10:29:57 -0400

    individual database for school

--- omar/database.php
+++ omar/database.php
@@ -30,3 +30,9 @@ define('__DB_NAME', "admin_democol");
 // define('__USERNAME', "admin_cosey_c");
 // define('__PASSWORD', "Amc123456");
 // define('__DB_NAME', "admin_cosey_c");
+
+/* ---------------------- Personal Database for School ---------------------- */
+// define('__HOST', "localhost");
+// define('__USERNAME', "admin_cst");
+// define('__PASSWORD', "Amc123456");
+// define('__DB_NAME', "admin_cst");
