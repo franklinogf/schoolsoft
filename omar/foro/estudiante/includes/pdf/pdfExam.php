a85c7f09e2ef1044e4ec7301e95a682471f944bb
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-05-25 10:30:13 -0400

    Utf8 decode

--- omar/foro/estudiante/includes/pdf/pdfExam.php
+++ omar/foro/estudiante/includes/pdf/pdfExam.php
@@ -33,21 +33,20 @@ $teacher = new Teacher($exam->id_maestro);
 $student = new Student(Session::id());
 $doneExam = $student->doneExam($exam->id);
 $pdf = new PDF();
-$pdf->SetTitle(utf8_decode(utf8_decode($exam->titulo)) . " - " . $student->fullName());
+$pdf->SetTitle(utf8_decode($exam->titulo) . " - " . $student->fullName(), true);
 $pdf->AddPage();
 $pdf->SetAutoPageBreak(true, 10);
-
-// Util::dump($exam);
-$points = $doneExam->puntos + $doneExam->bono;
+$pdf->headerFirstPage = true;
+$points = $doneExam->puntos + $doneExam->bonos;
 
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(20, 7, "Nombre: ", 0, 0, "L");
-$pdf->Cell(80, 7, $student->fullName(), 'B', 0, 'L');
+$pdf->Cell(80, 7, utf8_decode($student->fullName()), 'B', 0, 'L');
 $pdf->Cell(25, 7, '', 0, 0, 'L');
 $pdf->Cell(18, 7, 'Fecha: ', 0, 0, 'L');
 $pdf->Cell(35, 7, Util::formatDate($doneExam->terminado_el, true, true), 'B', 1, 'C');
 $pdf->Ln(5);
-$pdf->Cell(100, 7, utf8_decode($teacher->fullName()), 0, 0, "L");
+$pdf->Cell(100, 7, utf8_decode($teacher->fullName()) . $doneExam->bono, 0, 0, "L");
 $pdf->Cell(25, 7, '', 0, 0, 'L');
 $pdf->Cell(18, 7, 'Grado: ', 0, 0, 'L');
 $pdf->Cell(35, 7, $student->grado, 'B', 1, 'C');
@@ -77,7 +76,7 @@ if (isset($exam->fvs->topics)) {
 	$pdf->SetFont('Arial', '', 12);
 	$count = 1;
 	foreach ($exam->fvs->topics as $topic) {
-		$done = DB::table('T_examen_terminado_fyv',!__COSEY)->where([
+		$done = DB::table('T_examen_terminado_fyv', !__COSEY)->where([
 			['id_examen', $doneExam->id],
 			['id_pregunta', $topic->id]
 		])->first();
@@ -88,7 +87,7 @@ if (isset($exam->fvs->topics)) {
 		}
 		$pdf->Cell(10, 5, "$count.", 0, 0, "R");
 		$pdf->Cell(10, 4, strtoupper($done->respuesta), 'B', 0, 'C');
-		$pdf->MultiCell(0, 5, utf8_decode($topic->pregunta), 0, 1);
+		$pdf->MultiCell(0, 5, utf8_decode(utf8_decode($topic->pregunta)), 0, 1);
 		$pdf->Ln(2);
 		$count++;
 		black();
@@ -103,12 +102,13 @@ if (isset($exam->selects->topics)) {
 	$pdf->SetFont('Arial', '', 12);
 	$count = 1;
 	foreach ($exam->selects->topics as $topic) {
-		$done = DB::table('T_examen_terminado_selec',!__COSEY)->where([
+		if ($pdf->GetY() > 250) $pdf->addPage();
+		$done = DB::table('T_examen_terminado_selec', !__COSEY)->where([
 			['id_examen', $doneExam->id],
 			['id_pregunta', $topic->id]
 		])->first();
 		$pdf->Cell(10, 5, "$count.", 0, 0, "R");
-		$pdf->MultiCell(0, 5, utf8_decode($topic->pregunta));
+		$pdf->MultiCell(0, 5, utf8_decode(utf8_decode($topic->pregunta)));
 		$pdf->Ln(3);
 		for ($i = 1; $i <= 8; $i++) {
 			if (!empty($topic->{"respuesta{$i}"})) {
@@ -121,9 +121,9 @@ if (isset($exam->selects->topics)) {
 					}
 				}
 				$pdf->Cell(10, 5, "$i)", 0, 0, "R");
-				$pdf->Cell(0, 5, utf8_decode($topic->{"respuesta{$i}"}), 0, 1);
+				$pdf->Cell(0, 5, utf8_decode(utf8_decode($topic->{"respuesta{$i}"})), 0, 1);
+				black();
 			}
-			black();
 		}
 		$pdf->Ln(5);
 		$count++;
@@ -143,11 +143,11 @@ if (isset($exam->pairs->topics)) {
 		$lettersArray[$answer->id] = Exam::$letters[$index + 1];
 	}
 	foreach ($exam->pairs->topics as $topic) {
-		$done = DB::table('T_examen_terminado_parea',!__COSEY)->where([
+		$done = DB::table('T_examen_terminado_parea', !__COSEY)->where([
 			['id_examen', $doneExam->id],
 			['id_pregunta', $topic->id]
 		])->first();
-		$done2 = DB::table('T_examen_codigo_parea',!__COSEY)->find($done->respuesta);
+		$done2 = DB::table('T_examen_codigo_parea', !__COSEY)->find($done->respuesta);
 		$response = $topic->respuesta_c;
 		if ($response == $done2->id) {
 			green();
@@ -156,16 +156,18 @@ if (isset($exam->pairs->topics)) {
 		}
 		$pdf->Cell(10, 5, "$count.", 0, 0, "R");
 		$pdf->Cell(10, 4, $lettersArray[$done2->id], 'B', 0, 'C');
-		$pdf->MultiCell(90, 5, utf8_decode($topic->pregunta), 0, 1);
+		$pdf->MultiCell(90, 5, utf8_decode(utf8_decode($topic->pregunta)), 0, 1);
 		$count++;
 	}
+	$Y2 = $pdf->GetY();
 	black();
 	$pdf->SetY($Y);
 	foreach ($exam->pairCodes->topics as $index => $answer) {
 		$pdf->Cell(110);
-		$pdf->MultiCell(80, 5, $lettersArray[$answer->id] . ') ' . utf8_decode($answer->respuesta));
+		$pdf->MultiCell(80, 5, $lettersArray[$answer->id] . ') ' . utf8_decode(utf8_decode($answer->respuesta)));
 	}
 	$topicNumber++;
+	$pdf->SetY($Y2);
 }
 if (isset($exam->lines->topics)) {
 	$pdf->Ln(10);
@@ -175,7 +177,7 @@ if (isset($exam->lines->topics)) {
 	$pdf->SetFont('Arial', '', 12);
 	$count = 1;
 	foreach ($exam->lines->topics as $index => $topic) {
-		$done = DB::table('T_examen_terminado_linea',!__COSEY)->where([
+		$done = DB::table('T_examen_terminado_linea', !__COSEY)->where([
 			['id_examen', $doneExam->id],
 			['id_pregunta', $topic->id]
 		])->first();
@@ -184,7 +186,7 @@ if (isset($exam->lines->topics)) {
 		$words = explode(" ", $quest);
 		foreach ($words as $word) {
 			if ($word !== '~') {
-				$pdf->Cell($pdf->GetStringWidth($word) + 1, 5, utf8_decode($word));
+				$pdf->Cell($pdf->GetStringWidth($word) + 1, 5, utf8_decode(utf8_decode($word)));
 			} else {
 				$answerNumber = $index + 1;
 				if ($topic->{"respuesta{$answerNumber}"} == $done->{"respuesta{$answerNumber}"}) {
@@ -192,7 +194,7 @@ if (isset($exam->lines->topics)) {
 				} else {
 					red();
 				}
-				$pdf->Cell($pdf->GetStringWidth($done->{"respuesta{$answerNumber}"}) + 2, 5, utf8_decode($done->{"respuesta{$answerNumber}"}), 'B');
+				$pdf->Cell($pdf->GetStringWidth($done->{"respuesta{$answerNumber}"}) + 2, 5, utf8_decode(utf8_decode($done->{"respuesta{$answerNumber}"})), 'B');
 			}
 			black();
 		}
@@ -202,20 +204,22 @@ if (isset($exam->lines->topics)) {
 	$topicNumber++;
 }
 if (isset($exam->qas->topics)) {
-	$pdf->Ln(10);
+	if ($topicNumber > 1) $pdf->addPage();
 	$pdf->SetFont('Arial', 'B', 13);
 	$pdf->MultiCell(0, 15, utf8_decode("{$topicNumber} - {$exam->qas->title} ({$exam->qas->value})"), 0, 1);
 	$pdf->Ln(5);
-	$pdf->SetFont('Arial', '', 12);
 	$count = 1;
 	foreach ($exam->qas->topics as $topic) {
-		$done = DB::table('T_examen_terminado_pregunta',!__COSEY)->where([
+		$done = DB::table('T_examen_terminado_pregunta', !__COSEY)->where([
 			['id_examen', $doneExam->id],
 			['id_pregunta', $topic->id]
 		])->first();
+		$pdf->SetFont('Arial', 'B', 12);
+
 		$pdf->Cell(10, 5, "$count.", 0, 0, "R");
-		$pdf->MultiCell(0, 5, utf8_decode($topic->pregunta), 0, 1);
+		$pdf->MultiCell(0, 5, utf8_decode(utf8_decode($topic->pregunta)), 0, 1);
 		$pdf->Ln(3);
+		$pdf->SetFont('Arial', '', 12);
 		$pdf->MultiCell(0, 5, utf8_decode($done->respuesta), 0, 1);
 		$pdf->Ln(4);
 		$count++;
