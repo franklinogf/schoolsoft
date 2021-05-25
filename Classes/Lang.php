0fb2717a823cb04a3cbbfe9a974d8d42a0cf8808
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-05-25 10:26:53 -0400

    Changed the way its initialize

--- Classes/Lang.php
+++ Classes/Lang.php
@@ -7,31 +7,42 @@ class Lang
 
     private static $translation = [];
     private static $menuTranslation = [];
-    public static $select = (__LANG === 'es') ? 'Seleccionar' : 'Select';
-    public static $continue = (__LANG === 'es') ? 'Continuar' : 'Continue';
-    public static $grade = (__LANG === 'es') ? 'Curso' : 'Grade';
-    public static $score = (__LANG === 'es') ? 'Nota' : 'Grade';
-    public static $trimester = (__LANG === 'es') ? 'Trimestre' : 'Trimester';
-    public static $page = (__LANG === 'es') ? 'Pagina' : 'Page';
+    public static $select = '';
+    public static $continue = '';
+    public static $grade = '';
+    public static $score = '';
+    public static $trimester = '';
+    public static $page = '';
 
+    private static function init()
+    {
+        self::$select = (__LANG === 'es') ? 'Seleccionar' : 'Select';
+        self::$continue = (__LANG === 'es') ? 'Continuar' : 'Continue';
+        self::$grade = (__LANG === 'es') ? 'Curso' : 'Grade';
+        self::$score = (__LANG === 'es') ? 'Nota' : 'Grade';
+        self::$trimester = (__LANG === 'es') ? 'Trimestre' : 'Trimester';
+        self::$page = (__LANG === 'es') ? 'Pagina' : 'Page';
+    }
     public static function addTranslation($translationArray)
     {
+        self::init();
         self::$translation = $translationArray[__LANG];
     }
 
     public static function addMenutranslation($translationArray)
     {
+        self::init();
         self::$menuTranslation = $translationArray[__LANG];
     }
 
     public static function translation($text)
     {
         if (self::$translation !== null) {
-           if(isset(self::$translation[$text])){
-               return self::$translation[$text];
-           }else{
-            return "No hay traducción para {$text}";
-           }
+            if (isset(self::$translation[$text])) {
+                return self::$translation[$text];
+            } else {
+                return "No hay traducción para {$text}";
+            }
         }
         throw new \Exception("Primero se debe de agregar un array con las traducciones");
     }
@@ -39,11 +50,11 @@ class Lang
     public static function menuTranslation($text)
     {
         if (self::$menuTranslation !== null) {
-           if(isset(self::$menuTranslation[$text])){
-               return self::$menuTranslation[$text];
-           }else{
-            return "No hay traducción para {$text}";
-           }
+            if (isset(self::$menuTranslation[$text])) {
+                return self::$menuTranslation[$text];
+            } else {
+                return "No hay traducción para {$text}";
+            }
         }
         throw new \Exception("Primero se debe de agregar un array con las traducciones del menu");
     }
@@ -55,9 +66,6 @@ class Lang
 
     public static function plural($text)
     {
-        return $text.'s';
+        return $text . 's';
     }
-
-
-    
 }
