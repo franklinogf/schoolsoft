a758ddc17d800900668c97a6ce7359a684e05d6c
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-07-16 15:28:55 -0400

    added phone companies

--- Classes/Util.php
+++ Classes/Util.php
@@ -6,23 +6,23 @@ class Util
 {
    public static function formatDate($date, $month = false, $largeMonth = false)
    {
-      if($date !== '0000-00-00'){
+      if ($date !== '0000-00-00') {
          $format = '%d-%m-%Y';
-      if ($month) {
-         if (is_bool($month)) {
-            $format = '%d %b %Y';
-            if ($largeMonth) {
-               $format = '%d %B %Y';
+         if ($month) {
+            if (is_bool($month)) {
+               $format = '%d %b %Y';
+               if ($largeMonth) {
+                  $format = '%d %B %Y';
+               }
+            } else if (is_string($month)) {
+               $format = $month;
+            } else {
+               throw new \Exception('Introducir "true" o un formato de fecha correcto como (%d-%m-%Y)');
             }
-         } else if (is_string($month)) {
-            $format = $month;
-         } else {
-            throw new \Exception('Introducir "true" o un formato de fecha correcto como (%d-%m-%Y)');
          }
-      }
-      \setlocale(LC_ALL, 'es_ES');
-      $newDate = strftime($format, strtotime($date));
-      return $newDate;
+         \setlocale(LC_ALL, 'es_ES');
+         $newDate = strftime($format, strtotime($date));
+         return $newDate;
       }
    }
 
@@ -34,7 +34,7 @@ class Util
    public static function time($format = false)
    {
       $date = \date('H:i:s');
-      if($format) $date = self::formatTime($date);
+      if ($format) $date = self::formatTime($date);
 
       return $date;
    }
@@ -56,30 +56,35 @@ class Util
       $date = date('Y-m-d', strtotime("+{$numberOfDays} days", strtotime(date('Y-m-d'))));
       return $date;
    }
-  
+
 
    public static function formatTime($time)
    {
-      if(strpos($time, '(') > -1){
+      if (strpos($time, '(') > -1) {
          return $time;
       }
       $newTime = \date('g:i:s A', strtotime($time));
       return $newTime;
    }
 
-   public static function dump($var,$exit = true)
+   public static function dump($var, $exit = true)
    {
       echo '<pre>';
       \print_r($var);
       echo '<pre/>';
-      if($exit) exit();
+      if ($exit) exit();
    }
    public static function toObject($obj)
    {
       return json_decode(json_encode($obj));
-   }  
+   }
    public static function toJson($obj)
    {
       return json_encode($obj);
-   }  
+   }
+
+   public static function phoneCompanies()
+   {
+      return ["AT&T", "T-Movil", "Sprint", "Open M.", "Claro", "Verizon", "Suncom", "Boost"];
+   }
 }
