0f8360905ac897943d6178107dba81375ff44312
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-08-10 13:14:01 -0400

    moved virtual list to a other folder

--- omar/foro/profesor/reports/virtualList.php
+++ omar/foro/profesor/reports/virtualList.php
@@ -32,28 +32,30 @@ $teacher = new Teacher(Session::id());
     Route::includeFile('/foro/profesor/includes/tables/tableClasses.php');
     ?>
     <!-- virtual classes table -->
-    <table class="virtualClassesTable table table-striped table-hover cell-border w-100 shadow">
-      <thead class="bg-gradient-primary bg-primary border-0">
-        <tr>
-          <th>Titulo</th>
-          <th>Fecha</th>
-          <th>Hora</th>
-        </tr>
-      </thead>
-      <tbody>
-        
-      </tbody>
-      <tfoot>
-        <tr class="bg-gradient-secondary bg-secondary">
-          <th>Titulo</th>
-          <th>Fecha</th>
-          <th>Hora</th>
-        </tr>
-        <tr class="bg-gradient-light bg-light">
-          <td colspan="3"><button id="back" type="button" class="btn btn-block btn-primary">Atrás</button></td>
-        </tr>
-      </tfoot>
-    </table>
+    <div class="table_wrap">
+      <table class="virtualClassesTable table table-striped table-hover cell-border w-100 shadow">
+        <thead class="bg-gradient-primary bg-primary border-0">
+          <tr>
+            <th>Titulo</th>
+            <th>Fecha</th>
+            <th>Hora</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+        <tfoot>
+          <tr class="bg-gradient-secondary bg-secondary">
+            <th>Titulo</th>
+            <th>Fecha</th>
+            <th>Hora</th>
+          </tr>
+          <tr class="bg-gradient-light bg-light">
+            <td colspan="3"><button id="back" type="button" class="btn btn-block btn-primary">Atrás</button></td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
 
 
 
