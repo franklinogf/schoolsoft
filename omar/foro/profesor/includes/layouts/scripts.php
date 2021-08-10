91663e264736f093024251edf23353491475cdb1
Franklin Gonzalez <franklinomarflores@gmail.com>
2021-08-10 13:12:47 -0400

    updated to a new version

--- omar/foro/profesor/includes/layouts/scripts.php
+++ omar/foro/profesor/includes/layouts/scripts.php
@@ -4,12 +4,15 @@ global $DataTable;
 
 use Classes\Route;
 ?>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
 
 <?php if ($jqUI) Route::jqUI(); ?>
 <?php if ($DataTable) Route::includeFile('/includes/datatable-js.php', true); ?>
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 <?php
