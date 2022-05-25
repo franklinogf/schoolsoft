d760ee4900cad496246a405e7a24b7105067d3c1
Franklin Gonzalez <franklinomarflores@gmail.com>
2022-05-25 13:36:03 -0400

    Mail now can be send by others besides school and teacher

--- Classes/Mail.php
+++ Classes/Mail.php
@@ -15,43 +15,45 @@ class Mail extends PHPMailer
    public function __construct($debug = false, $type = 'School')
    {
       
-      if ($type === 'School') {
-         $school = new School();
-         $isSMTP = $school->info('host') === 'E' ? true : false;
-         $email = $school->info('correo');
-         $name = $school->info('colegio');
-         $replayToEmail = $email;
-         $replayToName = $name;
-         $host = $school->info('host_smtp');
-         $username = $school->info('email_smtp');
-         $password = $school->info('clave_email');
-         $port = $school->info('port');
-      } else {
-         $teacher = new Teacher(Session::id());
-         $isSMTP = $teacher->host === 'E' ? true : false;
-         $email = !$isSMTP ? $teacher->email1 : $teacher->info('email_smtp');
-         $name = !$isSMTP ? $teacher->fullName() : $teacher->info('colegio');
-         $replayToEmail = $teacher->email1;
-         $replayToName = $teacher->fullName();
-         $host = $teacher->info('host_smtp');
-         $username = $teacher->info('email_smtp');
-         $password = $teacher->info('clave_email');
-         $port = $teacher->info('port');
+      if($type === 'School' || $type === 'Teacher'){
+         if ($type === 'School') {
+            $school = new School();
+            $isSMTP = $school->info('host') === 'E' ? true : false;
+            $email = $school->info('correo');
+            $name = $school->info('colegio');
+            $replayToEmail = $email;
+            $replayToName = $name;
+            $host = $school->info('host_smtp');
+            $username = $school->info('email_smtp');
+            $password = $school->info('clave_email');
+            $port = $school->info('port');
+         } else {
+            $teacher = new Teacher(Session::id());
+            $isSMTP = $teacher->host === 'E' ? true : false;
+            $email = !$isSMTP ? $teacher->email1 : $teacher->info('email_smtp');
+            $name = !$isSMTP ? $teacher->fullName() : $teacher->info('colegio');
+            $replayToEmail = $teacher->email1;
+            $replayToName = $teacher->fullName();
+            $host = $teacher->info('host_smtp');
+            $username = $teacher->info('email_smtp');
+            $password = $teacher->info('clave_email');
+            $port = $teacher->info('port');
+         }
+   
+         if ($isSMTP) {
+            parent::__construct(true);
+            $this->SMTPDebug = ($debug) ? SMTP::DEBUG_SERVER : SMTP::DEBUG_OFF;
+            $this->isSMTP();
+            $this->Host       = $host;
+            $this->SMTPAuth   = true;
+            $this->Username   = $username;
+            $this->Password   = $password;
+            $this->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $this->Port       =  $port;
+         }
+   
+         $this->setFrom($email, utf8_decode($name));
+         if ($this->replyTo)  $this->addReplyTo($replayToEmail, utf8_decode($replayToName));
       }
-
-      if ($isSMTP) {
-         parent::__construct(true);
-         $this->SMTPDebug = ($debug) ? SMTP::DEBUG_SERVER : SMTP::DEBUG_OFF;
-         $this->isSMTP();
-         $this->Host       = $host;
-         $this->SMTPAuth   = true;
-         $this->Username   = $username;
-         $this->Password   = $password;
-         $this->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-         $this->Port       =  $port;
-      }
-
-      $this->setFrom($email, utf8_decode($name));
-      if ($this->replyTo)  $this->addReplyTo($replayToEmail, utf8_decode($replayToName));
    }
 }
