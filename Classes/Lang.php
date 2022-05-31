6605b60edc9be951ff9a544eddcc0becfcfa4fbb
Franklin Gonzalez <franklinomarflores@gmail.com>
2022-05-31 18:09:52 -0400

    Change The way the translations works

--- Classes/Lang.php
+++ Classes/Lang.php
@@ -5,63 +5,69 @@ namespace Classes;
 class Lang
 {
 
-    private static $translation = [];
-    private static $menuTranslation = [];
-    public static $trans = [];
-   
+    private $translations = [];
 
-    private static function init()
+     public function __construct(array $translation = [])
     {
-        self::$trans['select'] = (__LANG === 'es') ? 'Seleccionar' : 'Select';
-        self::$trans['continue'] = (__LANG === 'es') ? 'Continuar' : 'Continue';
-        self::$trans['grade'] = (__LANG === 'es') ? 'Curso' : 'Grade';
-        self::$trans['score'] = (__LANG === 'es') ? 'Nota' : 'Grade';
-        self::$trans['trimester'] = (__LANG === 'es') ? 'Trimestre' : 'Trimester';
-        self::$trans['page'] = (__LANG === 'es') ? 'Pagina' : 'Page';
-    }
-    public static function addTranslation($translationArray)
-    {
-        self::init();
-        self::$translation = $translationArray[__LANG];
-    }
-
-    public static function addMenutranslation($translationArray)
-    {
-        self::init();
-        self::$menuTranslation = $translationArray[__LANG];
-    }
-
-    public static function translation($text)
-    {
-        if (self::$translation !== null) {
-            if (isset(self::$translation[$text])) {
-                return self::$translation[$text];
-            } else {
-                return "No hay traducción para {$text}";
-            }
+        if (count($translation) > 0) {
+            $this->addTranslation($translation);
         }
-        throw new \Exception("Primero se debe de agregar un array con las traducciones");
+        $defaultTranslations = [
+            ['Seleccionar', 'Select'],
+            ['Continuar', 'Continue'],
+            ['Curso', 'Grade'],
+            ['Nota', 'Grade'],
+            ['Trimestre', 'Trimester'],
+            ['Pagina', 'Page'],
+            ['Derechos reservados', 'Copy Right'],
+        ];
+        $this->addTranslation($defaultTranslations);
     }
 
-    public static function menuTranslation($text)
+    private function addTranslation(array $translationArray)
     {
-        if (self::$menuTranslation !== null) {
-            if (isset(self::$menuTranslation[$text])) {
-                return self::$menuTranslation[$text];
-            } else {
-                return "No hay traducción para {$text}";
-            }
+        foreach ($translationArray as $translation) {
+            $this->translations[strtolower($translation[0])]['es'] = $translation[0];
+            $this->translations[strtolower($translation[0])]['en'] = $translation[1];
         }
-        throw new \Exception("Primero se debe de agregar un array con las traducciones del menu");
     }
 
-    public static function translate($spanishText, $englishText)
+    public function translation($text)
     {
-        return (__LANG === 'es') ? $spanishText : $englishText;
+        return $this->translations[strtolower($text)][__LANG];
     }
 
-    public static function plural($text)
-    {
-        return $text . 's';
-    }
+    // public static function translation($text)
+    // {
+    //     if (self::$translation !== null) {
+    //         if (isset(self::$translation[$text])) {
+    //             return self::$translation[$text];
+    //         } else {
+    //             return "No hay traducción para {$text}";
+    //         }
+    //     }
+    //     throw new \Exception("Primero se debe de agregar un array con las traducciones");
+    // }
+
+    // public static function menuTranslation($text)
+    // {
+    //     if (self::$menuTranslation !== null) {
+    //         if (isset(self::$menuTranslation[$text])) {
+    //             return self::$menuTranslation[$text];
+    //         } else {
+    //             return "No hay traducción para {$text}";
+    //         }
+    //     }
+    //     throw new \Exception("Primero se debe de agregar un array con las traducciones del menu");
+    // }
+
+    // public static function translate($spanishText, $englishText)
+    // {
+    //     return (__LANG === 'es') ? $spanishText : $englishText;
+    // }
+
+    // public static function plural($text)
+    // {
+    //     return $text . 's';
+    // }
 }
