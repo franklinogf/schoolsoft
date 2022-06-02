b3dc97b532718f0324d0f2332c59d47a0a7777cc
Franklin Gonzalez <franklinomarflores@gmail.com>
2022-06-02 15:17:21 -0400

    set default translations to be overwrited

--- Classes/Lang.php
+++ Classes/Lang.php
@@ -8,10 +8,7 @@ class Lang
     private $translations = [];
 
     public function __construct(array $translation = [])
-    {
-        if (count($translation) > 0) {
-            $this->addTranslation($translation);
-        }
+    {       
         $defaultTranslations = [
             ['Seleccionar', 'Select'],
             ['Continuar', 'Continue'],
@@ -20,21 +17,31 @@ class Lang
             ['Trimestre', 'Trimester'],
             ['Pagina', 'Page'],
             ['Derechos reservados', 'Copy Right'],
+            ["Notas","Grades"],
+            ["Notas de verano","Summer grades"],
+            ["Pruebas cortas","Short tests"],
+            ["Trabajos diarios","Daily homework"],
+            ["Trabajos de libreta","Book homework"],
+            ["Conducta y asistencia","Behavior and attendance"],
+            ["Examen final","Final exam"],
         ];
         $this->addTranslation($defaultTranslations);
+        if (count($translation) > 0) {
+            $this->addTranslation($translation);
+        }
     }
     // can be used after initialization
     public function addTranslation(array $translationArray)
     {
         foreach ($translationArray as $translation) {
-            $this->translations[strtolower($translation[0])]['es'] = $translation[0];
-            $this->translations[strtolower($translation[0])]['en'] = $translation[1];
+            $this->translations[strtolower(utf8_decode($translation[0]))]['es'] = utf8_decode($translation[0]);
+            $this->translations[strtolower(utf8_decode($translation[0]))]['en'] = $translation[1];
         }
     }
 
     public function translation($text)
     {
-        return $this->translations[strtolower($text)][__LANG];
+        return $this->translations[strtolower(utf8_decode($text))][__LANG];
     }
 
     // public static function translation($text)
