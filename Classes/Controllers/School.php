c3ec5809c858c26f73215f9a9fc0247c47d87d33
Franklin Gonzalez <franklinomarflores@gmail.com>
2022-08-29 11:23:33 -0400

    Added admin login and school save method

--- Classes/Controllers/School.php
+++ Classes/Controllers/School.php
@@ -8,12 +8,26 @@ use Classes\Models\SchoolModel;
 
 class School extends SchoolModel
 {
-
    protected $props = [];
+
+   public function login($username, $password)
+   {
+
+      if ($array = $this->adminLogin($username, $password)) {
+         foreach ($array as $key => $value) {
+            $this->{$key} = $value;
+         }
+
+         $this->logged = true;
+      } else {
+
+         $this->logged = false;
+      }
+      return $this;
+   }
 
    public function __construct($user = 'administrador')
    {
-
       $array = $this->getSchoolByUser($user);
       foreach ($array as $key => $value) {
          $this->set($key, $value);
@@ -35,5 +49,20 @@ class School extends SchoolModel
       $newPath = ($root ? __ROOT : '');
       $newPath .= __LOGO_PATH . $path;
       return $newPath;
+   }
+   public function save()
+   {
+      // get self public class, no parents classes
+      $propsArray[] = array_diff_key(get_object_vars($this), get_class_vars(get_parent_class($this)));
+      
+      if (count($propsArray[0]['props']) > 0) {
+         if (isset($this->props[$this->primary_key])) {
+            $this->updateAdmin($propsArray[0]['props']);
+         } else {
+            echo 'insert <hr>';
+         }
+      } else {
+         throw new \Exception('Debe de asignar valor a las propiedades en primer lugar');
+      }
    }
 }
