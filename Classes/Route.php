d1c2b2851325a650cc0d88af9baec0c23c57ed28
Franklin Gonzalez <franklinomarflores@gmail.com>
2022-08-29 11:20:54 -0400

    Added select picker include

--- Classes/Route.php
+++ Classes/Route.php
@@ -42,7 +42,11 @@ class Route
          return false;
       }
    }
-
+   public static function selectPicker($type = 'css')
+   {
+      echo $type === 'css' ? '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">'
+         : '<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>';
+   }
    public static function jqUI()
    {
       echo '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>';
