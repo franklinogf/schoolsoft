d043b9d2c2eb8243b951e8bf5f1f572c201437a9
Franklin Gonzalez <franklinomarflores@gmail.com>
2022-08-29 11:22:11 -0400

    added cafeteria login

--- Classes/Login.php
+++ Classes/Login.php
@@ -3,13 +3,14 @@
 namespace Classes;
 
 use Classes\Controllers\Parents;
+use Classes\Controllers\School;
 use Classes\Controllers\Student;
 use Classes\Controllers\Teacher;
 
 class Login
 {
    private static $errorLoginMessage = "No coincide con los datos, intentelo otra vez";
-   
+
    public static function login($request, $location)
    {
       self::{$location}($request);
@@ -31,6 +32,42 @@ class Login
          Route::redirect();
       }
    }
+   private static function admin($request)
+   {
+      $school = new School();
+      if ($school->login(trim($request['username']), trim($request['password']))->logged === true) {
+
+         $_SESSION['logged'] = [
+            'location' => "admin",
+            "user" => ['id' => $school->usuario],
+         ];
+         $_SESSION['id1'] = $school->id;
+         $_SESSION['usua1'] = $school->usuario;
+         Route::redirect('/');
+      } else {
+         $_SESSION['errorLogin'] = self::$errorLoginMessage;
+
+         Route::redirect();
+      }
+   }
+   private static function cafeteria($request)
+   {
+      $school = new School();
+      if ($school->login(trim($request['username']), trim($request['password']))->logged === true) {
+
+         $_SESSION['logged'] = [
+            'location' => "cafeteria",
+            "user" => ['id' => $school->usuario],
+         ];
+         $_SESSION['id1'] = $school->id;
+         $_SESSION['usua1'] = $school->usuario;
+         Route::redirect('/menu.php');
+      } else {
+         $_SESSION['errorLogin'] = self::$errorLoginMessage;
+
+         Route::redirect();
+      }
+   }
    private static function regiweb($request)
    {
       $teacher = new Teacher();
@@ -39,7 +76,6 @@ class Login
          $_SESSION['logged'] = [
             'location' => "regiweb",
             "user" => ['id' => $teacher->id],
-            'type' => 'profesor'
          ];
          Route::redirect('/');
       } else {
