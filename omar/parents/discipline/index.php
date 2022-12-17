ff2619e9163289893e82335fd3795a89d64c0c25
Franklin Gonzalez <franklinomarflores@gmail.com>
2022-12-17 11:52:41 -0500

    parent disciple folder done

---
+++ omar/parents/discipline/index.php
@@ -0,0 +1,66 @@
+<?php
+require_once '../../app.php';
+
+use Classes\Controllers\Parents;
+use Classes\Lang;
+use Classes\Route;
+use Classes\Session;
+
+Session::is_logged();
+$parents = new Parents(Session::id());
+$lang = new Lang([
+    ["Disciplina", "Discipline"],
+    ["Yo, padre, madre o encargado, certifico que veré las Disciplina de mi hijo(a) a través del Internet. El sistema guardará un recibo en evidencia de que ví las Disciplina en curso.", "I, father, mother or in charge, I certify that I will see the Discipline of my child through the Internet.The system will keep an evidence that I saw the Discipline."],
+    ["He leido y estoy de acuerdo.", "I have read and I agree."],
+    ["Estudiantes inscritos", "Registered students"]
+]);
+?>
+<!DOCTYPE html>
+<html lang="<?= __LANG ?>">
+
+<head>
+    <?php
+    $title = $lang->translation("Disciplina");
+    Route::includeFile('/parents/includes/layouts/header.php');
+    ?>
+</head>
+
+<body>
+    <?php
+    Route::includeFile('/parents/includes/layouts/menu.php');
+    ?>
+    <div class="container mt-3">
+        <h1 class="text-center my-2"><?= $lang->translation("Disciplina") ?></h1>
+        <?php if ($parents->activo == 'Activo') : ?>
+            <div class="jumbotron mt-3">
+                <p><?= $lang->translation("Yo, padre, madre o encargado, certifico que veré las Disciplina de mi hijo(a) a través del Internet. El sistema guardará un recibo en evidencia de que ví las Disciplina en curso.") ?></p>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="accept">
+                    <label class="custom-control-label" for="accept"><?= $lang->translation("He leido y estoy de acuerdo.") ?></label>
+                </div>
+            </div>
+            <form action="<?= Route::url('/parents/discipline/pdfDiscipline.php') ?>" method="POST" target="_blank">
+                <div class="form-row">
+                    <label class="font-weight-bold col-12" for="studentSS"><?= $lang->translation("Estudiantes inscritos") ?></label>
+                    <select name="studentSS" id="studentSS" class="form-control col-12 col-lg-6">
+                        <?php foreach ($parents->kids() as $kid) : ?>
+                            <option value="<?= $kid->ss ?>"><?= "$kid->nombre $kid->apellidos -> $kid->grado" ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+                <button id="gradesBtn" type="submit" class="btn btn-primary mt-3" disabled><?= $lang->translation("Disciplina") ?></button>
+            </form>
+        <?php else : ?>
+            <div class="jumbotron mt-3 border-danger">
+                <div class="alert alert-danger" role="alert">
+                    <?= $parents->info('men_inac') ?>
+                </div>
+            </div>
+        <?php endif ?>
+    </div>
+    <?php
+    Route::includeFile('/includes/layouts/scripts.php', true);
+    ?>
+</body>
+
+</html>
---
+++ omar/parents/discipline/pdfDiscipline.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once '../../app.php';
+
+use Classes\PDF;
+use Classes\Lang;
+use Classes\Util;
+use Classes\Session;
+use Classes\DataBase\DB;
+use Classes\Controllers\Student;
+
+Session::is_logged();
+$studentSS = $_POST['studentSS'];
+$student = new Student($studentSS);
+$year = $student->info('year');
+
+
+$lang = new Lang([
+    ['Lista de disciplinas aplicadas', 'Applied Discipline List'],
+    ['Nombre','Name'],
+    ['Fecha','Date'],
+    ['Titulo','Title'],
+    ['Cantidad','Amount'],
+    ['Tallies','Tallies'],
+    ['Méritos','Mertis'],
+    ['Deméritos','Demerits'],
+]);
+
+DB::table('acuse')->insert([
+    'id'=> $student->id,
+    'ss' => $studentSS,
+    'grado' => $student->grado,
+    'year' => $year,
+    'ip' => Util::getIp(),
+    'fecha' => Util::date(),
+    'hora' => Util::time(),
+    'tri' => '',
+    'cn' => '',
+    'tra' => '',
+    'tri2' => '',
+    'fra' => '',
+    'hoja' => '5'
+]);
+$pdf = new PDF();
+$pdf->SetTitle($lang->translation("Lista de disciplinas aplicadas"));
+$pdf->Fill();
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->addPage();
+
+$pdf->Cell(0, 5, $lang->translation("Lista de disciplinas aplicadas"), 0, 1, 'C');
+$pdf->Ln(5);
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(40, 5, $lang->translation("Nombre"), 1, 0, 'C', true);
+$pdf->Cell(70, 5, $student->nombre.' '.$student->apellidos, 1, 0);
+$pdf->Cell(30, 5, $lang->translation("Fecha"), 1, 0, 'C', true);
+$pdf->Cell(40, 5, date('m-d-Y'), 1, 1);
+
+$pdf->Ln(5);
+$pdf->Cell(100, 5, $lang->translation("Titulo"), 1, 0, 'C', true);
+$pdf->Cell(40, 5, $lang->translation("Fecha"), 1, 0, 'C', true);
+$pdf->Cell(40, 5, $lang->translation("Cantidad"), 1, 1, 'C', true);
+$memos = DB::table("memos")->where([
+    ['ss', $student->ss],
+    ['year', $year]
+])->get();
+$amounts = [];
+foreach ($memos as $memo) {
+    $pdf->Cell(100, 5, $memo->titulo, 1);
+    $pdf->Cell(40, 5, $memo->fecha, 1, 0, 'C');
+    $pdf->Cell(40, 5, $memo->demeritos, 1, 1, 'C');
+    if (strpos($memo->titulo, "Merits") > -1) {
+        $amounts['merits'] += $memo->demeritos;
+    } elseif (strpos($memo->titulo, "Tallies") > -1) {
+        $amounts['tallies'] += $memo->demeritos;
+    } else {
+        $amounts['demertis'] += $memo->demeritos;
+    }
+}
+$pdf->Ln(5);
+
+$pdf->Cell(40, 5, 'Tallies', 1, 0, 'L', true);
+$pdf->Cell(20, 5, $amounts['tallies'], 1, 1, 'R');
+$pdf->Cell(40, 5, 'Merits', 1, 0, 'L', true);
+$pdf->Cell(20, 5, $amounts['merits'], 1, 1, 'R');
+$pdf->Cell(40, 5, 'Demerits', 1, 0, 'L', true);
+$pdf->Cell(20, 5, $amounts['demertis'], 1, 1, 'R');
+
+
+$pdf->Output();
