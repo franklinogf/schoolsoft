eef71069308915aefa4488c8ba851d04223751ff
Franklin Gonzalez <franklinomarflores@gmail.com>
2023-03-07 15:59:38 -0500

    only if it has a targer

--- omar/admin/users/index.php
+++ omar/admin/users/index.php
@@ -186,7 +186,7 @@ $lang = new Lang([
                             <div class="row row-cols-2">
                                 <?php foreach ($option['buttons'] as $button) : ?>
                                     <div class="col mt-1">
-                                        <a style="font-size: .8em;" title="<?= $button['desc'][__LANG] ?>" <?= $button['target'] ? "target='{$button['target']}'" : '' ?> class="btn btn-primary btn-block" href="<?= $button['link'] ?>"><?= mb_strtoupper($button['name'][__LANG], 'UTF-8') ?></a>
+                                        <a style="font-size: .8em;" title="<?= $button['desc'][__LANG] ?>" <?= isset($button['target']) ? "target='{$button['target']}'" : '' ?> class="btn btn-primary btn-block" href="<?= $button['link'] ?>"><?= mb_strtoupper($button['name'][__LANG], 'UTF-8') ?></a>
                                     </div>
                                 <?php endforeach; ?>
                             </div>
