bff4b16c54d0f1b7ceee74d5f08d17d3baadebab
Alfredo <alf_med@hotmail.com>
2023-12-22 12:17:45 -0500

    Cambios en la pantalla

--- omar/admin/access/reports/AdminAccessReport.php
+++ omar/admin/access/reports/AdminAccessReport.php
@@ -8,36 +8,40 @@ use Classes\Session;
 use Classes\Controllers\School;
 use Classes\Controllers\Student;
 use Classes\Controllers\Teacher;
+use Classes\DataBase\DB;
 
 Session::is_logged();
 $teacher = new Teacher();
 $lang = new Lang([
-    ['Informe de acceso de los padres', 'Parents Access Report'],
+    ['Informe de acceso de los administradores', 'Administrator Access Report'],
     ['Desde', 'From'],
     ['Hasta', 'To'],
     ['Grado', 'Grade'],
     ['Grados separados', 'Separted grades'],
     ['estudiante', 'student'],
-    ['Atrás', 'Go back'],
-    ['Opción', 'Option'],
+    ['AtrÃ¡s', 'Go back'],
+    ['OpciÃ³n', 'Option'],
     ['Por estudiante', 'By student'],
     ['Por grado', 'By grade'],
-    ['Estudiante', 'Student'],
-    ['Selección', 'Selection'],
+    ['Administradores', 'Administrator'],
+    ['SelecciÃ³n', 'Selection'],
     ['Lista', 'List'],
-    ['Resumen', 'Summary'],
+    ['Todos', 'All'],
 ]);
 $students = new Student();
 $allStudents = $students->all();
 $school = new School();
 $grades = $school->allGrades();
+
+$admins = DB::table('colegio')->orderBy('usuario')->get();
+
 ?>
 <!DOCTYPE html>
 <html lang="<?= __LANG ?>">
 
 <head>
     <?php
-    $title = $lang->translation('Informe de acceso de los padres');
+    $title = $lang->translation('Informe de acceso de los administradores');
     Route::includeFile('/admin/includes/layouts/header.php');
     Route::selectPicker();
     ?>
@@ -48,8 +52,8 @@ $grades = $school->allGrades();
     Route::includeFile('/admin/includes/layouts/menu.php');
     ?>
     <div class="container-lg mt-lg-3 mb-5 px-0">
-        <h1 class="text-center my-3"><?= $lang->translation('Informe de acceso de los padres') ?></h1>
-        <a href="<?= Route::url('/admin/access/reports/') ?>" class="btn btn-secondary mb-2"><?= $lang->translation("Atrás") ?></a>
+        <h1 class="text-center my-3"><?= $lang->translation('Informe de acceso de los administradores') ?></h1>
+        <a href="<?= Route::url('/admin/access/reports/') ?>" class="btn btn-secondary mb-2"><?= $lang->translation("Atr&#65533;s") ?></a>
         <div class="container bg-white shadow-lg py-3 rounded">
             <form action="<?= Route::url('/admin/access/reports/pdf/ParentsAccessReport.php') ?>" target="ParentsAccessReport" method="POST">
                 <div class="mx-auto" style="width: 25rem;">
@@ -65,24 +69,17 @@ $grades = $school->allGrades();
                         </div>
                         <input id="to" class="form-control" type="date" name="to" value="<?= Util::date() ?>" required>
                     </div>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <label class='input-group-text' for="option"><?= $lang->translation("Opción") ?></label>
-                        </div>
-                        <select name="option" id="option" class="form-control">
-                            <option value="student"><?= $lang->translation("Por estudiante") ?></option>
-                            <option value="grade"><?= $lang->translation("Por grado") ?></option>
-                        </select>
-                    </div>
+
                     <div id="student">
                         <div class="input-group mb-2">
                             <div class="input-group-prepend">
                                 <label class='input-group-text' for="student"><?= $lang->translation("Estudiante") ?></label>
                             </div>
                             <select class="form-control selectpicker w-100" name="student" data-live-search="true" required>
-                                <option value=""><?= $lang->translation("Seleccionar") . ' ' . $lang->translation('estudiante') ?></option>
-                                <?php foreach ($allStudents as $student) : ?>
-                                    <option value="<?= $student->mt ?>"><?= "$student->apellidos $student->nombre ($student->id)" ?></option>
+                                <option value=""><?= $lang->translation("Seleccionar") . ' ' . $lang->translation('Administradores') ?></option>
+                                <option value="all"><?= $lang->translation("Todos") ?></option>
+                                <?php foreach ($admins as $student) : ?>
+                                    <option value="<?= $student->id ?>"><?= "$student->usuario ($student->id)" ?></option>
                                 <?php endforeach ?>
                             </select>
                         </div>
@@ -93,7 +90,7 @@ $grades = $school->allGrades();
                                 <label class='input-group-text' for="option"><?= $lang->translation("Grado") ?></label>
                             </div>
                             <select name="grade" class="form-control">
-                                <option value="7"><?= $lang->translation("Selección") ?></option>
+                                <option value="7"><?= $lang->translation("SelecciÃ³n") ?></option>
                                 <?php foreach ($grades as $grade) : ?>
                                     <option value="<?= $grade ?>"><?= $grade ?></option>
                                 <?php endforeach ?>
