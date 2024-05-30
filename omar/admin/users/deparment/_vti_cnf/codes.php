8f8cfb671afc8fabcbcb1c275d2f260c641add2f
Alfredo <alf_med@hotmail.com>
2024-05-30 15:06:49 -0400

    departamentos entrada

---
+++ omar/admin/users/deparment/_vti_cnf/codes.php
@@ -0,0 +1,18 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|30 May 2024 18:59:32 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|SchoolSoft\\Alfreddo
+vti_modifiedby:SR|SchoolSoft\\Alfreddo
+vti_timecreated:TR|03 Apr 2023 12:51:39 -0000
+vti_backlinkinfo:VX|
+vti_syncwith_schoolsoftpr.org\:21:TW|30 May 2024 18:59:32 -0000
+vti_syncofs_schoolsoftpr.org\:21:TW|30 May 2024 19:59:41 -0000
+vti_nexttolasttimemodified:TW|30 May 2024 18:49:33 -0000
+vti_cacheddtm:TX|30 May 2024 18:59:32 -0000
+vti_filesize:IR|3982
+vti_cachedbodystyle:SR|<body>
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
