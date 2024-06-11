b165a6ac5ef352f5a4a14dac5859edfe4a932134
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-06-11 11:58:43 -0400

    new alerts

--- js/alerts.js
+++ js/alerts.js
@@ -1,21 +1,30 @@
 const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
     toast.onmouseenter = Swal.stopTimer;
     toast.onmouseleave = Swal.resumeTimer;
-    }
+  },
 });
 
-
-
 const ConfirmationAlert = Swal.mixin({
-    icon: "warning",
-    title: __LANG === 'es' ? 'Seguro que desea borrarlo?' : 'Are you sure you want to delete it?',
-    confirmButtonText: __LANG === 'es' ? "Aceptar" : 'Accept',
-    showCancelButton: true,
-    cancelButtonText: __LANG === 'es' ? "Cancelar" : 'Cancel'
+  icon: "warning",
+  confirmButtonText: __LANG === "es" ? "Aceptar" : "Accept",
+  showCancelButton: true,
+  cancelButtonText: __LANG === "es" ? "Cancelar" : "Cancel",
+  customClass: {
+    confirmButton: "btn btn-primary mr-2",
+    cancelButton: "btn btn-secondary",
+  },
+  buttonsStyling: false,
+});
+const Alert = Swal.mixin({
+  confirmButtonText: __LANG === "es" ? "Aceptar" : "Accept",
+  customClass: {
+    confirmButton: "btn btn-primary",
+  },
+  buttonsStyling: false,
 });
