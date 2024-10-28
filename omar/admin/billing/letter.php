5e31124221fa9b5bd15d9a2475b9c121d69e53f8
Alfredo <alf_med@hotmail.com>
2024-10-28 15:59:40 -0400

    Se cambio el formato de la lista azul a columnas separadas

--- omar/admin/billing/letter.php
+++ omar/admin/billing/letter.php
@@ -26,7 +26,7 @@ $lang = new Lang([
 $school = new School(Session::id());
 $year = $school->info('year2');
 $students = new Student();
-$students = $students->all();
+//$students = $students->all();
 
 $debe = 0;
 foreach ($students as $student) {
@@ -67,49 +67,38 @@ $students = DB::table('year')
     <div class="container-lg mt-lg-3 mb-5 px-0">
         <h1 class="text-center mb-3 mt-5"><?= $lang->translation('Lista de deudores') . ' ' . $year ?> </h1>
         <div class="container mt-1">
-            <form action="pdf/letter_inf.php" method="post" target="_blank">
-                <div class="table_wrap">
-                    <table class="dataTable table table-sm table-pointer table-striped table-hover cell-border shadow">
-                        <thead class="bg-gradient-primary bg-primary border-0">
-                            <tr class="checkbox">
-                                <th style=" width: 1px;">
-                                    <div class="custom-control custom-checkbox">
-                                        <input class="custom-control-input bg-success checkAll" type="checkbox" id="check1">
-                                        <label class="custom-control-label" for="check1"></label>
-                                    </div>
-                                </th>
-                                <th><?= $lang->translation("Apellidos") ?></th>
-                                <th><?= $lang->translation("Nombre") ?></th>
-                                <th><?= $lang->translation("Grado") ?></th>
-                                <th>ID</th>
-                                <th><?= $lang->translation("Deudas") ?></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($students as $student) :
-                                $parent = new Parents($student->id);
-                            ?>
-                                <tr>
-                                    <td>
-                                        <div class="custom-control custom-checkbox">
-                                            <input id="student_<?= $student->mt ?>" class="custom-control-input check" type="checkbox" data-id="<?= $student->id ?>" <?= ($parent->activo !== 'Activo') ? 'checked=""' : '' ?>>
-                                            <label class="custom-control-label" for="student_<?= $student->mt ?>"></label>
-                                        </div>
-                                    </td>
-                                    <td><?= $student->nombre ?></td>
-                                    <td><?= $student->apellidos ?></td>
-                                    <td><?= $student->grado ?></td>
-                                    <td><?= $student->id ?></td>
-                                    <td><?= $student->tr1 ?></td>
-                                </tr>
-                            <?php endforeach ?>
-                        </tbody>
-                    </table>
-                </div>
+            <form id="form" action="pdf/letter_inf.php" method="post" target="studentID" target="_blank">
+                <?php
+                $students = DB::table('year')->where([['tr1', '>', 0], ['year', $year], ['activo', '']])->orderBy('tr1 DESC, apellidos')->get();
+                $__tableData = $students;
+                //            $__tableData = DB::table('year')->where([['tr1','>', 0], ['year', $year], ['activo', '']])->orderBy('tr1 DESC, apellidos')->get();
+                $__tableDataCheckbox = true; #decirle que quiere usar los check box
+                $__dataPk = 'ss'; #el identificador principal
+                // Un array de las columnas y sus respectivos valores
+                $__tableDataInfo = [
+                    [
+                        'title' => ['es' => 'Apellidos', 'en' => 'Last name'],
+                        'values' => ['apellidos']
+                    ],
+                    [
+                        'title' => ['es' => 'Nombre', 'en' => 'Name'],
+                        'values' => ['nombre']
+                    ],
+                    [
+                        'title' => ['es' => 'Grado', 'en' => 'Grade'],
+                        'values' => ['grado']
+                    ],
+                    [
+                        'title' => ['es' => 'Deuda pendiente', 'en' => 'Outstanding debt'],
+                        'values' => ['tr1']
+                    ],
+                ];
+                Route::includeFile('/includes/layouts/table.php', true);
+                ?>
                 <div><b>
                         <center>
                             <span lang="en-us"><?= $lang->translation("Enviar por E-mail") ?></span>
-                            <input name="m6" type="checkbox" value="Si" style="height: 25px; width: 25px"><br />
+                            <input name="correo" type="checkbox" value="Si" style="height: 25px; width: 25px"><br />
                             <span lang="en-us"><?= $lang->translation("Se enviaron por correo electrónico todas las deudas que están marcadas en la lista.") ?></span>
                         </center>
                     </b></div>
@@ -117,69 +106,15 @@ $students = DB::table('year')
             </form>
         </div>
     </div>
-
     <?php
     $DataTable = true;
     Route::includeFile('/includes/layouts/scripts.php', true);
     ?>
-    <script type="text/javascript">
+    <script>
         $(document).ready(function() {
-            $("#save").on('click', function(e) {
-                $.post(includeThisFile(), {
-                    save: true,
-                    message: $("#message").val(),
-                    lock: $("#lock").val(),
-                    expireDay: $("#expireDay").val(),
-                    automaticLock: $("#automaticLock").val(),
-                    code1: $("#code1").val(),
-                    code2: $("#code2").val(),
-                    code3: $("#code3").val(),
-                }, function(data, textStatus, xhr) {
-                    $('#alert').show()
-                });
-
-            })
-            $('#optionsModal').on('show.bs.modal', function(e) {
-                $('#alert').hide()
-
-                $.post(includeThisFile(), {
-                    search: true,
-                }, function(data, textStatus, xhr) {
-                    console.log(data);
-                    $("#message").val(data.message)
-                    $("#lock").val(data.lock)
-                    $("#expireDay").val(data.expireDay)
-                    $("#automaticLock").val(data.automaticLock)
-                    $("#code1").val(data.code1)
-                    $("#code2").val(data.code2)
-                    $("#code3").val(data.code3)
-                    $("#automaticLock").change()
-                }, 'json');
-            })
-            $("#expireDay").change(function() {
-                if ($(this).val() > 30) {
-                    $(this).val(30);
-                } else if ($(this).val() < 1) {
-                    $(this).val(1);
-                }
+            $("#form").submit(function(e) {
+                tableDataToSubmit("#form", dataTable[0], 'students[]')
             });
-
-            $("#automaticLock").on('change', function(e) {
-                $("#code1,#code2,#code3").prop('disabled', $(this).val() === 'Si' ? false : true);
-            });
-
-            $(".custom-control-input.check").on('change', function(e) {
-                const id = $(this).data('id')
-                const value = !$(this).prop('checked') ? 'Activo' : 'Inactivo'
-
-                $.post(includeThisFile(), {
-                    check: id,
-                    value,
-                }, function(data, textStatus, xhr) {
-                    console.log('data:', data);
-                });
-            });
-
         });
     </script>
 
