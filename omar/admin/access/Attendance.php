3f49268e99ecdeb71ee469020423616177bcf6ed
Alfredo <alf_med@hotmail.com>
2024-11-07 15:57:49 -0500

    Se añadio opcion de entrada de asistencia

--- omar/admin/access/Attendance.php
+++ omar/admin/access/Attendance.php
@@ -22,11 +22,19 @@ $lang = new Lang([
     ['Nombre', 'Name'],
     ['Todos', 'All'],
     ['Guardar', 'Save'],
+    ['Salón Hogar', 'Home Room'],
+    ['Aplicar por asistencia', 'Apply for assistance'],
+    ['Grados', 'Degrees'],
+    ['Materias', 'Subjects'],
+    ['Asistencia diaria por', 'Daily assistance by'],
+    ['', ''],
+    ['', ''],
 ]);
-$years = DB::table('year')->select("DISTINCT year")->get();
 $school = new School();
-$teachers = new Teacher();
-$teachers = $teachers->all();
+
+$colegio = DB::table('colegio')->where([
+    ['usuario', 'administrador']
+])->orderBy('id')->first();
 
 ?>
 <!DOCTYPE html>
@@ -65,7 +73,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater1_start"><?= $lang->translation('Inicio') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater1_start" id="quater1_start" value="<?= $school->info('asis1') ?>">
+                                    <input type="date" class="form-control" name="quater1_start" id="quater1_start" value="<?= $colegio->asis1 ?>">
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6">
@@ -73,7 +81,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater1_end"><?= $lang->translation('Cierre') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater1_end" id="quater1_end" value="<?= $school->info('asis2') ?>">
+                                    <input type="date" class="form-control" name="quater1_end" id="quater1_end" value="<?= $colegio->asis2 ?>">
                                 </div>
                             </div>
                             <div class="col-12">
@@ -84,7 +92,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater2_start"><?= $lang->translation('Inicio') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater2_start" id="quater2_start" value="<?= $school->info('asis3') ?>">
+                                    <input type="date" class="form-control" name="quater2_start" id="quater2_start" value="<?= $colegio->asis3 ?>">
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6">
@@ -92,7 +100,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater2_end"><?= $lang->translation('Cierre') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater2_end" id="quater2_end" value="<?= $school->info('asis4') ?>">
+                                    <input type="date" class="form-control" name="quater2_end" id="quater2_end" value="<?= $colegio->asis4 ?>">
                                 </div>
                             </div>
                         </div>
@@ -107,7 +115,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater3_start"><?= $lang->translation('Inicio') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater3_start" id="quater3_start" value="<?= $school->info('asis5') ?>">
+                                    <input type="date" class="form-control" name="quater3_start" id="quater3_start" value="<?= $colegio->asis5 ?>">
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6">
@@ -115,7 +123,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater3_end"><?= $lang->translation('Cierre') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater3_end" id="quater3_end" value="<?= $school->info('asis6') ?>">
+                                    <input type="date" class="form-control" name="quater3_end" id="quater3_end" value="<?= $colegio->asis6 ?>">
                                 </div>
                             </div>
                             <div class="col-12">
@@ -126,7 +134,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater4_start"><?= $lang->translation('Inicio') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater4_start" id="quater4_start" value="<?= $school->info('asis7') ?>">
+                                    <input type="date" class="form-control" name="quater4_start" id="quater4_start" value="<?= $colegio->asis7 ?>">
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6">
@@ -134,7 +142,7 @@ $teachers = $teachers->all();
                                     <div class="input-group-prepend">
                                         <label class="input-group-text" for="quater4_end"><?= $lang->translation('Cierre') ?></label>
                                     </div>
-                                    <input type="date" class="form-control" name="quater4_end" id="quater4_end" value="<?= $school->info('asis8') ?>">
+                                    <input type="date" class="form-control" name="quater4_end" id="quater4_end" value="<?= $colegio->asis8 ?>">
                                 </div>
                             </div>
                         </div>
@@ -143,23 +151,23 @@ $teachers = $teachers->all();
                     <div class="col-12 border p-2 mt-2">
                         <div class="row">
                             <div class="col-12 col-lg-2">
-                                <p><?= $lang->translation('Verano') ?></p>
+                                <p><?= $lang->translation('Asistencia diaria por') ?></p>
+                            </div>
+                            <select class="form-control" name="asist" id="asist" required style="width: 140px">
+                                <option <?= $colegio->asist == '1' ? 'selected=""' : '' ?> value="1"><?= utf8_encode($lang->translation("Salón Hogar")) ?></option>
+                                <option <?= $colegio->asist == '2' ? 'selected=""' : '' ?> value="2"><?= $lang->translation("Grados") ?></option>
+                                <option <?= $colegio->asist == '3' ? 'selected=""' : '' ?> value="3"><?= $lang->translation("Materias") ?></option>
+                            </select><span lang="en-us">&nbsp;&nbsp;&nbsp;
+                            </span>&nbsp;<div class="col-12 col-lg-2">
+                                <p><?= $lang->translation('Aplicar por asistencia') ?></p>
+                            </div>
+                            <select class="form-control" name="asis" id="asis" required style="width: 140px">
+                                <option <?= $colegio->asis == 'A' ? 'selected=""' : '' ?> value="A"><?= utf8_encode($lang->translation("Diaria")) ?></option>
+                                <option <?= $colegio->asis == 'B' ? 'selected=""' : '' ?> value="B"><?= $lang->translation("Trimestral") ?></option>
+                            </select>
+                            <div class="col-12 col-lg-5">
                             </div>
                             <div class="col-12 col-lg-5">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <label class="input-group-text" for="summer_start"><?= $lang->translation('Inicio') ?></label>
-                                    </div>
-                                    <input type="date" class="form-control" name="summer_start" id="summer_start" value="<?= $school->info('fechav1') ?>">
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-5">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <label class="input-group-text" for="summer_end"><?= $lang->translation('Cierre') ?></label>
-                                    </div>
-                                    <input type="date" class="form-control" name="summer_end" id="summer_end" value="<?= $school->info('fechav2') ?>">
-                                </div>
                             </div>
                         </div>
                     </div>
