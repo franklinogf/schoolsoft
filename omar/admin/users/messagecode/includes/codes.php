a01323da6d120da057cfed9dbf5fe330565f0f8f
Alfredo <alf_med@hotmail.com>
2024-11-15 10:15:04 -0500

    Para control de la tabla codigos

---
+++ omar/admin/users/messagecode/includes/codes.php
@@ -0,0 +1,34 @@
+<?php
+require_once '../../../../app.php';
+
+use Classes\DataBase\DB;
+use Classes\Server;
+use Classes\Session;
+
+Session::is_logged();
+Server::is_post();
+if (isset($_POST['addCode'])) {
+    $code = $_POST['code'];
+    $description = $_POST['description'];
+    $description2 = $_POST['description2'];
+
+    DB::table('codigos')->insert([
+        'codigo' => $code,
+        't1e' => $description,
+        't2e' => $description2,
+    ]);
+
+} else if (isset($_POST['editCode'])) {
+    $code = $_POST['code'];
+    $description2 = $_POST['description2'];
+    $description = $_POST['description'];
+
+    DB::table('codigos')->where(['codigo', $_POST['editCode']])->update([
+        'codigo' => $code,
+        't1e' => $description,
+        't2e' => $description2,
+    ]);
+} else if (isset($_POST['deleteCode'])) {
+    $code = $_POST['code'];
+    DB::table('codigos')->where(['codigo', $code])->delete();
+}
