00e27d2c6952d67ee46b3b58c5e7f0e074641b2d
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-01-09 23:20:24 -0500

    Refactor Mail class constructor and queue method for improved readability and structure

--- Classes/Mail.php
+++ Classes/Mail.php
@@ -2,68 +2,71 @@
 
 namespace Classes;
 
-use Classes\DataBase\DB;
-use Classes\Mail\SMTP;
-use Classes\Mail\PHPMailer;
 use Classes\Controllers\School;
 use Classes\Controllers\Teacher;
-use Classes\Mail\Exception;
+use Classes\DataBase\DB;
+use Classes\Mail\PHPMailer;
+use Classes\Mail\SMTP;
 
 class Mail extends PHPMailer
 {
-   public $replyTo = true;
+    public $replyTo = true;
 
-   public function __construct($debug = false, $type = 'School')
-   {
+    public function __construct($debug = false, $type = 'School')
+    {
+        $this->CharSet = 'UTF-8';
 
-      if ($type === 'School' || $type === 'Teacher') {
-         $school = new School();
-         $isSMTP = $school->info('host') === 'E' ? true : false;
-         $name = $school->info('colegio');
-         $fromEmail = $replayToEmail = $school->info('correo');
-         $replayToName = $name;
-         if ($type === 'Teacher') {
-            $teacher = new Teacher(Session::id());
-            $replayToEmail = $teacher->email1;
-            $replayToName = $teacher->fullName();
-         }
-         if ($isSMTP) {
-            $host = $school->info('host_smtp');
-            $fromEmail = $school->info('email_smtp');
-            $password = $school->info('clave_email');
-            $port = $school->info('port');
-            parent::__construct(true);
-            $this->isSMTP();
-            $this->SMTPDebug = ($debug) ? SMTP::DEBUG_SERVER : SMTP::DEBUG_OFF;
-            $this->Host = $host;
-            $this->SMTPAuth = true;
-            $this->Username = $fromEmail;
-            $this->Password = $password;
-            $this->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $this->Port = $port;
-         }
+        if ($type === 'School' || $type === 'Teacher') {
+            $school = new School();
+            $isSMTP = $school->info('host') === 'E' ? true : false;
+            $name = $school->info('colegio');
+            $fromEmail = $replayToEmail = $school->info('correo');
+            $replayToName = $name;
+            if ($type === 'Teacher') {
+                $teacher = new Teacher(Session::id());
+                $replayToEmail = $teacher->email1;
+                $replayToName = $teacher->fullName();
+            }
+            if ($isSMTP) {
+                $host = $school->info('host_smtp');
+                $fromEmail = $school->info('email_smtp');
+                $password = $school->info('clave_email');
+                $port = $school->info('port');
+                parent::__construct(true);
+                $this->isSMTP();
+                $this->SMTPDebug = ($debug) ? SMTP::DEBUG_SERVER : SMTP::DEBUG_OFF;
+                $this->Host = $host;
+                $this->SMTPAuth = true;
+                $this->Username = $fromEmail;
+                $this->Password = $password;
+                $this->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $this->Port = $port;
+            }
 
-         $this->setFrom($fromEmail, utf8_decode($name));
-         if ($this->replyTo)
-            $this->addReplyTo($replayToEmail, utf8_decode($replayToName));
-      }
-   }
+            $this->setFrom($fromEmail, utf8_decode($name));
+            if ($this->replyTo) {
+                $this->addReplyTo($replayToEmail, utf8_decode($replayToName));
+            }
 
-   public static function queue(string $from, string $reply_to, array $to, string $subject, string $message, string|null $text = null, array $attachments = [])
-   {
-      if (count($to) === 0) {
-         return;
-      }
+        }
+    }
 
-      DB::table('email_queue')->insert([
-         'from' => $from,
-         'to' => json_encode($to),
-         'reply_to' => $reply_to,
-         'message' => $message,
-         'text' => $text,
-         'subject' => $subject,
-         'attachments' => json_encode($attachments, JSON_UNESCAPED_UNICODE),
-         'user' => Session::id() ?? null,
-      ]);
-   }
+    public static function queue(string $from, string $replyTo, array $to, string $subject, string $message, string | null $text = null, array $attachments = [])
+    {
+        if (count($to) === 0) {
+            return;
+        }
+
+        DB::table('email_queue')->insert([
+            'from' => $from,
+            'to' => json_encode($to),
+            'reply_to' => $replyTo,
+            'message' => $message,
+            'text' => $text,
+            'subject' => $subject,
+            'attachments' => json_encode($attachments, JSON_UNESCAPED_UNICODE),
+            'user' => Session::id() ?? null,
+        ]);
+    }
+
 }
