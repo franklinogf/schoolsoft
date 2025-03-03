0f9b493f1c22383046ce7893cc887d69b3acc88e
Alfredo <alf_med@hotmail.com>
2025-03-03 10:29:48 -0500

    tarjetas

--- omar/admin/access/gradesReports/pdf/TarjetaOpciones.php
+++ omar/admin/access/gradesReports/pdf/TarjetaOpciones.php
@@ -2,3 +2,15 @@
 if ($_POST['tarjeta'] == '1') {
     require_once('TarjetaNotas1.php');
 }
+if ($_POST['tarjeta'] == '2') {
+    require_once('TarjetaNotas2.php');
+}
+if ($_POST['tarjeta'] == '3') {
+    require_once('TarjetaNotas3.php');
+}
+if ($_POST['tarjeta'] == '7') {
+    require_once('TarjetaNotas7.php');
+}
+if ($_POST['tarjeta'] == '14') {
+    require_once('TarjetaNotas14.php');
+}
