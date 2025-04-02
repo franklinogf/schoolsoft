b190d80999fa2fabb0793804018afd8b8e94926b
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-02 15:30:58 -0400

    Add Bootstrap 5 CSS and JS includes to Route class

--- Classes/Route.php
+++ Classes/Route.php
@@ -58,7 +58,16 @@ class Route
    {
       echo '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>';
    }
+   //boostrap 5
+   public static function boostrapCss()
+   {
+      echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">';
+   }
 
+   public static function boostrapJs()
+   {
+      echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>';
+   }
    public static function fontawasome()
    {
       echo '<script src="https://kit.fontawesome.com/f4bf4b6549.js" crossorigin="anonymous"></script>';
