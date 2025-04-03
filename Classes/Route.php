d87ebff2834bfbf870e3c9755a9abda40356f48c
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-02 20:50:50 -0400

    Add bootstrapCSS method and remove deprecated bootstrap methods

--- Classes/Route.php
+++ Classes/Route.php
@@ -29,6 +29,15 @@ class Route
       echo '<link rel="stylesheet" href="' . $root . $path . '" />';
    }
 
+   public static function bootstrapCSS(): string
+   {
+      $path = __SCHOOL_URL . '/css/main-bootstrap.css';
+      if (!file_exists(__ROOT_SCHOOL . '/css/main-bootstrap.css')) {
+         $path = '/css/main-bootstrap.css';
+      }
+      return "<link rel='stylesheet' href='{$path}'/>";
+   }
+
    public static function js($path, $serverRoot = false)
    {
       $root = '';
@@ -57,16 +66,6 @@ class Route
    public static function jqUI()
    {
       echo '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>';
-   }
-   //boostrap 5
-   public static function boostrapCss()
-   {
-      echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">';
-   }
-
-   public static function boostrapJs()
-   {
-      echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>';
    }
    public static function fontawasome()
    {
