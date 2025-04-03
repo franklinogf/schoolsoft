773e7757c36465c93ec2f183b333be440696fcc2
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-02 20:51:01 -0400

    Add theme configuration file with color settings and responsive options

---
+++ config/theme.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "theme-colors" => [
+        "primary" => "#59ABE3",
+        "secondary" => "#6C7A89",
+        "light" => "#F0F1F3"
+    ],
+    "enable-shadows" => true,
+    "enable-responsive-font-sizes" => true,
+];
