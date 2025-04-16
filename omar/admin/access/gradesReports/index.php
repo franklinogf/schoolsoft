313f9ae71d042349f8d3a56b234f9c01f45cf2ff
Alfredo <alf_med@hotmail.com>
2025-04-16 11:29:14 -0400

    Se añadieron los permisos

--- omar/admin/access/gradesReports/index.php
+++ omar/admin/access/gradesReports/index.php
@@ -1,11 +1,15 @@
 <?php
 require_once '../../../app.php';
 
+use App\Enums\AdminPermission;
 use Classes\Lang;
 use Classes\Route;
 use Classes\Session;
+use App\Models\Admin;
 
 Session::is_logged();
+
+$user  = Admin::user(Session::id())->first();
 
 $options = [
     [
@@ -16,76 +20,91 @@ $options = [
                 'name' => ["es" => 'Tarjeta de notas', "en" => "Grade report"],
                 'desc' => ['es' => 'Pantalla para imprimir reporte de notas', 'en' => 'Screen to print grade report'],
                 'link' => 'TarjetaOpciones.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_NOTE_CARD)
             ],
             [
                 'name' => ["es" => 'Hoja de progreso', "en" => "Progress sheet"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => 'HojaPrograso.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_PROGRESS_SHEET)
             ],
             [
                 'name' => ["es" => 'Registro de notas', "en" => "Record of notes"],
                 'desc' => ['es' => 'Informes del registro de los maestros', 'en' => 'Teacher Record Reports'],
                 'link' => 'RegistroNotas.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_NOTE_REGISTRY)
             ],
             [
                 'name' => ["es" => 'Distribucción de notas', "en" => "Note distribution"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => 'DistribuccionNotas.php ',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_NOTE_DISTRIBUTION)
             ],
             [
                 'name' => ["es" => 'Lista de fracasados', "en" => "List of failures"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => 'ListaFracasados.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_FAILED_LIST)
             ],
             [
                 'name' => ["es" => 'Lista de promedios', "en" => "List of averages"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => 'ListaPromedios.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_AVERAGE_LIST)
             ],
             [
                 'name' => ["es" => 'Notas en letras', "en" => "Notas en letras"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => '#',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_NOTES_IN_LETTERS)
             ],
             [
                 'name' => ["es" => 'Lista de rango', "en" => "Rank List"],
                 'desc' => ['es' => 'Listado de rango por materias', 'en' => 'Rank list by materials'],
                 'link' => 'ListaRango.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_RANK_LIST)
             ],
             [
                 'name' => ["es" => 'Rango por grado', "en" => "Rank by grade"],
                 'desc' => ['es' => 'Listado de rango por grado', 'en' => 'List of rank by grade'],
                 'link' => 'RangoGrado.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_GRADE_RANK)
             ],
             [
                 'name' => ["es" => 'Conducta', "en" => "Conduct"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => 'Conduct.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_AVERAGE_AND_BEHAVIOR)
             ],
             [
                 'name' => ["es" => 'Cambios en registro', "en" => "Registry changes"],
                 'desc' => ['es' => 'Cambio que los maestros hacen en el registro de notas', 'en' => 'Change that teachers make in the grade record'],
                 'link' => 'gradesChanges.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_REGIWEB_CHANGES)
             ],
             [
                 'name' => ["es" => 'Boleta de verano', "en" => "Summer card"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => 'SummerCard.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_SUMMER_REPORT)
             ],
             [
                 'name' => ["es" => 'Promedios decimales', "en" => "Promedios decimales"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => '#',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_PERCENT_TO_DECIMAL)
             ],
             [
                 'name' => ["es" => 'Cuadro de honor', "en" => "Honor roll"],
                 'desc' => ['es' => 'Listado por curso o materias para el cuadro de honor.', 'en' => 'List by course or subjects for the honor roll.'],
                 'link' => 'HonorRoll.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_HONOR_ROLL)
             ],
             [
                 'name' => ["es" => 'Inf. de Deficiencia', "en" => "Deficiency Report"],
                 'desc' => ['es' => 'Beleta de deficiencia por estudiante para los padres.', 'en' => 'Deficiency letter per student for parents.'],
                 'link' => 'Deficiencia.php',
+                'hidden' => !$user->hasPermissionTo(AdminPermission::GRADES_REPORTS_DEFICIENCY_REPORTS)
             ],
             [
                 'name' => ["es" => 'Notas finales', "en" => "Notas finales"],
@@ -162,11 +181,6 @@ $options = [
                 'link' => '#',
             ],
             [
-                'name' => ["es" => 'Deficiencia', "en" => "Deficiencia"],
-                'desc' => ['es' => '', 'en' => ''],
-                'link' => '#',
-            ],
-            [
                 'name' => ["es" => 'Asistencia', "en" => "Attendance"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => 'Attendance.php',
@@ -193,11 +207,6 @@ $options = [
                 'link' => '#',
             ],
             [
-                'name' => ["es" => 'Curriculum', "en" => "Curriculum"],
-                'desc' => ['es' => '', 'en' => ''],
-                'link' => '#',
-            ],
-            [
                 'name' => ["es" => 'Labor', "en" => "Labor"],
                 'desc' => ['es' => '', 'en' => ''],
                 'link' => '#',
@@ -214,23 +223,23 @@ $lang = new Lang([
 
 ?>
 <!DOCTYPE html>
-<html lang="<?=__LANG?>">
+<html lang="<?= __LANG ?>">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 
 <head>
     <?php
-$title = $lang->translation("Opciones");
-Route::includeFile('/admin/includes/layouts/header.php');
-?>
+    $title = $lang->translation("Opciones");
+    Route::includeFile('/admin/includes/layouts/header.php');
+    ?>
 </head>
 
 <body>
     <?php
-Route::includeFile('/admin/includes/layouts/menu.php');
-?>
+    Route::includeFile('/admin/includes/layouts/menu.php');
+    ?>
     <div class="container-md mt-md-3 mb-md-5 px-0">
         <h1 class="text-center my-3">
-            <?=$lang->translation("Opciones")?>
+            <?= $lang->translation("Opciones") ?>
         </h1>
 
         <div class="row row-cols-1 row-cols-md-2 mx-2 mx-md-0 justify-content-around">
@@ -239,22 +248,23 @@ Route::includeFile('/admin/includes/layouts/menu.php');
                 <div class="col mb-4">
                     <fieldset class="border border-secondary rounded-bottom h-100 px-2">
                         <legend class="w-auto">
-                            <?=$option['title'][__LANG]?>
+                            <?= $option['title'][__LANG] ?>
                         </legend>
                         <div class="pb-3">
                             <div class="row row-cols-2">
                                 <?php foreach ($option['buttons'] as $button): ?>
+                                    <?php if ($button['hidden']) continue  ?>
                                     <div class="col mt-1">
-                                        <a style="font-size: .8em;" title="<?=isset($button['desc']) ? $button['desc'][__LANG] : ''?>" <?=isset($button['target']) ? "target='{$button['target']}'" : ''?> class="btn btn-primary btn-block" href="<?=$button['link']?>">
-                                            <?=mb_strtoupper($button['name'][__LANG], 'UTF-8')?>
+                                        <a style="font-size: .8em;" title="<?= isset($button['desc']) ? $button['desc'][__LANG] : '' ?>" <?= isset($button['target']) ? "target='{$button['target']}'" : '' ?> class="btn btn-primary btn-block" href="<?= $button['link'] ?>">
+                                            <?= mb_strtoupper($button['name'][__LANG], 'UTF-8') ?>
                                         </a>
                                     </div>
-                                <?php endforeach;?>
+                                <?php endforeach; ?>
                             </div>
                         </div>
                     </fieldset>
                 </div>
-            <?php endforeach?>
+            <?php endforeach ?>
 
 
 
@@ -262,8 +272,8 @@ Route::includeFile('/admin/includes/layouts/menu.php');
     </div>
 
     <?php
-Route::includeFile('/includes/layouts/scripts.php', true);
-?>
+    Route::includeFile('/includes/layouts/scripts.php', true);
+    ?>
 </body>
 
 </html>
