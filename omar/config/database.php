c11cd6a1cb6b0e70d246bc1691ff6e6df89e3354
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-04-22 16:05:37 -0400

    Update database host configuration to use the correct IP address

--- omar/config/database.php
+++ omar/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => 'schoolsoftpr.org',
+    'host' => '82.197.82.10',
     'database' => 'u291111878_demo2',
     'username' => 'u291111878_demo2',
     'password' => 'Amc@3151',
