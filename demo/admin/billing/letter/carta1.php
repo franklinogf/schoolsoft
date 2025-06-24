56722a67c45612551054a33cf2815b55780a9701
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-06-24 01:49:45 -0400

    Refactor carta1.php to enhance email handling; replace direct mail logic with Email model for sending first collection notices and streamline PDF generation process.

--- demo/admin/billing/letter/carta1.php
+++ demo/admin/billing/letter/carta1.php
@@ -1,16 +1,22 @@
 <?php
+
+use App\Models\Family;
+
 require_once '../../../app.php';
 date_default_timezone_set('America/Puerto_Rico');
 
 use Classes\Controllers\School;
 use Classes\DataBase\DB;
+use Classes\Email;
 use Classes\Lang;
-use Classes\Mail;
 use Classes\PDF;
 use Classes\Session;
-
+use Illuminate\Support\Carbon;
 
 Session::is_logged();
+
+$today = Carbon::now();
+$files = [];
 $lang = new Lang([
     ['ESTADO DE CUENTAS', 'STATEMENT'],
     ['Primer aviso de cobro', 'First collection notice'],
@@ -48,7 +54,6 @@ $lang = new Lang([
     ['CORDIALMENTE', 'CORDIALLY'],
     ['OFICINA DE FINANZAS', 'FINANCE OFFICE'],
     ['Si usted ha realizado el pago antes mencionado, favor de hacer caso omiso a esta notificaci&#65533;n.', 'If you have made the aforementioned payment, please ignore this notification.'],
-    ['', ''],
 ]);
 
 $db = new DB();
@@ -77,29 +82,26 @@ $fechaFinal = "";
 if ($mes > 6) {
     $year1 = "{$year[0]}{$year[1]}";
     $fechaFinal = "";
-	// $fechaFinal = "AND fecha_d>='$year1-07-01'";
+    // $fechaFinal = "AND fecha_d>='$year1-07-01'";
 }
 class nPDF extends PDF
 {
-    function Header()
-    {
-        parent::header();
-    }
-    function Footer()
+    public function Footer()
     {
         global $lang;
         $this->SetFont('Arial', '', 11);
         $this->SetY(-40);
-        $this->Cell(0, 5, $lang->translation('CORDIALMENTE'), 0, 1);
+        $this->Cell(0, 5, __('CORDIALMENTE'), 0, 1);
         $this->Ln(8);
-        $this->Cell(0, 5, $lang->translation('OFICINA DE FINANZAS'));
+        $this->Cell(0, 5, __('OFICINA DE FINANZAS'));
         $this->Ln(10);
         $this->SetFont('Arial', 'B', 11);
-        $this->Cell(0, 5, utf8_encode($lang->translation('Si usted ha realizado el pago antes mencionado, favor de hacer caso omiso a esta notificaci&#65533;n.')));
+        $this->Cell(0, 5, __('Si usted ha realizado el pago antes mencionado, favor de hacer caso omiso a esta notificaci&#65533;n.'));
     }
 }
 
-$MES = array(
+
+$MES = [
     'enero',
     'febrero',
     'marzo',
@@ -112,12 +114,13 @@ $MES = array(
     'octubre',
     'noviembre',
     'diciembre'
-);
-$pdf = new nPDF();
+];
+
 $result = DB::table('pagos')->select("DISTINCT id")
     ->whereRaw("fecha_d <= '$fecha' AND year = '$year' and baja=''")->orderBy('id')->get();
 
 foreach ($result as $r) {
+    $pdf = new nPDF('P');
     $pdf->SetFont('Arial', '', 12);
     $rs = DB::table('pagos')->select("DISTINCT fecha_d")
         ->whereRaw("id='$r->id' AND fecha_d <= '$fecha' AND year = '$year' $fechaFinal and baja=''")->orderBy('fecha_d')->get();
@@ -136,16 +139,12 @@ foreach ($result as $r) {
         }
         if ($total != 0) {
             $pdf->AddPage();
-            $pdf->Cell(0, 5, $lang->translation('Primer aviso de cobro'), 0, 1);
+            $pdf->Cell(0, 5, __('Primer aviso de cobro'), 0, 1);
             $pdf->Ln(5);
-            if ($lang->translation('es') == 'es') {
-                $fechaHoy = date('j') . ' de ' . $MES[date('n') - 1] . ' de ' . date('Y');
-            } else {
-                $fechaHoy = $lang->translation($MES[date('n') - 1]) . ' ' . date('j') . ', ' . date('Y');
-            }
+            $fechaHoy = __LANG === 'es' ? $today->translatedFormat('j \d\e F \d\e Y') : $today->translatedFormat('F j, Y');
             $pdf->Cell(0, 5, $fechaHoy, 0, 1);
             $pdf->Ln(5);
-            $pdf->Cell(0, 5, $lang->translation('Padre, Madre o Encargado'), 0, 1);
+            $pdf->Cell(0, 5, __('Padre, Madre o Encargado'), 0, 1);
             $pdf->Ln(3);
             $pdf->SetFont('Arial', 'B', 12);
             $estu = DB::table('madre')
@@ -154,9 +153,9 @@ foreach ($result as $r) {
             $pdf->Cell(0, 5, $estu->padre ?? '', 0, 1);
             $pdf->Ln(3);
             $pdf->SetFont('Arial', '', 12);
-            $pdf->Cell(0, 5, $lang->translation('CUENTA') . ' # ' . $r->id, 0, 1);
+            $pdf->Cell(0, 5, __('CUENTA') . ' # ' . $r->id, 0, 1);
             $pdf->Ln(5);
-            $pdf->MultiCell(0, 6, $lang->translation('HEMOS REVISADO NUESTRAS CUENTAS A COBRAR Y ENCONTRAMOS QUE USTED A LA FECHA DE HOY ') . $fechaHoy . $lang->translation(' NO HA EFECTUADO EL PAGO CORRESPONDIENTE AL MES DE:'));
+            $pdf->MultiCell(0, 6, __('HEMOS REVISADO NUESTRAS CUENTAS A COBRAR Y ENCONTRAMOS QUE USTED A LA FECHA DE HOY ') . $fechaHoy . __(' NO HA EFECTUADO EL PAGO CORRESPONDIENTE AL MES DE:'));
             $pdf->Ln(5);
             $pdf->SetFont('Arial', 'B', 12);
             $TOTAL = 0;
@@ -172,7 +171,7 @@ foreach ($result as $r) {
                 $total = $deudas - $pagos;
                 $TOTAL += $total;
                 if ($total != 0) {
-                    $MES1 = strtoupper($MES[date('n', strtotime($rd->fecha_d)) - 1]);
+                    $MES1 = strtoupper(Carbon::parse($rd->fecha_d)->translatedFormat('F'));
                     list($y, $m, $d) = explode("-", $rd->fecha_d);
                     if ($m < 6) {
                         $y4 = ' 20' . $y2;
@@ -180,224 +179,55 @@ foreach ($result as $r) {
                         $y4 = ' 20' . $y3;
                     }
 
-                    if ($MES1 == $lang->translation('Junio') . ' ' . $y4) {
-                        $MES1 = $lang->translation('MATRICULA');
+                    if ($MES1 == __('Junio') . ' ' . $y4) {
+                        $MES1 = __('MATRICULA');
                     }
 
-                    $pdf->Cell(37, 5, $lang->translation($MES1) . ' ' . $y4);
+                    $pdf->Cell(37, 5, __($MES1) . ' ' . $y4);
                     $pdf->Cell(90, 5, '.......................................................................');
                     $pdf->Cell(20, 5, number_format($total, 2), 0, 1, 'R');
                 }
             }
-            $pdf->Cell(37, 5, $lang->translation('BALANCE'));
+            $pdf->Cell(37, 5, __('BALANCE'));
             $pdf->Cell(90, 5, '.......................................................................');
             $pdf->Cell(20, 5, number_format($TOTAL, 2), 0, 1, 'R');
             $pdf->SetFont('Arial', '', 11);
             $pdf->Ln(5);
-            $pdf->Cell(0, 5, $lang->translation('NOTA IMPORTANTE:'), 0, 1);
+            $pdf->Cell(0, 5, __('NOTA IMPORTANTE:'), 0, 1);
             $pdf->Ln(5);
-            $pdf->MultiCell(0, 7, $lang->translation('1. DESPUES DEL DIA 10 DE CADA MES SE COBRARAN $') . $chk . $lang->translation(' DE CARGOS POR DEMORA POR CUENTA.'));
+            $pdf->MultiCell(0, 7, __('1. DESPUES DEL DIA 10 DE CADA MES SE COBRARAN $') . $chk . __(' DE CARGOS POR DEMORA POR CUENTA.'));
             $pdf->Ln(3);
-            $pdf->MultiCell(0, 7, $lang->translation('2. LOS PAGOS PUEDEN HACERSE MEDIANTE TARJETA DE CREDITO, ATH, ATHMOVIL BUSINESS, EFECTIVO, GIRO POSTAL.'));
+            $pdf->MultiCell(0, 7, __('2. LOS PAGOS PUEDEN HACERSE MEDIANTE TARJETA DE CREDITO, ATH, ATHMOVIL BUSINESS, EFECTIVO, GIRO POSTAL.'));
             $pdf->Ln(3);
-            $pdf->MultiCell(0, 7, $lang->translation('3. FAVOR DE HACER LOS ARREGLOS PERTINENTES PARA QUE LOS SERVICIOS EDUCATIVOS DE SU HIJO(A) NO SE VEAN AFECTADOS.'));
+            $pdf->MultiCell(0, 7, __('3. FAVOR DE HACER LOS ARREGLOS PERTINENTES PARA QUE LOS SERVICIOS EDUCATIVOS DE SU HIJO(A) NO SE VEAN AFECTADOS.'));
             $pdf->Ln(3);
+            $filePath = __DIR__ . '/attachments/letter_' . $r->id . '.pdf';
+            $pdf->Output("F", $filePath);
+            $files[$r->id] = $filePath;
         }
     }
 }
 
-$pdf->Output();
+PDF::OutputFiles($files);
 
-if ($_POST['tipo'] == 'email') {
-    $result = DB::table('pagos')->select("DISTINCT id")
-        ->whereRaw("fecha_d <= '$fecha' AND year = '$year' and baja=''")->orderBy('id')->get();
-    //**********************************************
-
-    foreach ($result as $r) {
-
-        $pdf = new PDF();
-        $pdf->SetFont('Arial', '', 12);
-        $rs = DB::table('pagos')->select("DISTINCT fecha_d")
-            ->whereRaw("id='$r->id' AND fecha_d <= '$fecha' AND year = '$year' $fechaFinal and baja=''")->orderBy('fecha_d')->get();
-        $total = 0;
-        if (count($rs) > 0) {
-            foreach ($rs as $rd) {
-                $deudas = 0;
-                $pagos = 0;
-                $p = DB::table('pagos')
-                    ->whereRaw("fecha_d='$rd->fecha_d' AND id='$r->id' AND year = '$year' AND fecha_d <= '$fecha' $fechaFinal and baja=''")->get();
-                foreach ($p as $row) {
-                    $deudas += $row->deuda;
-                    $pagos += $row->pago;
-                }
-                $total = $total + $deudas - $pagos;
-            }
-            if ($total != 0) {
-                $pdf->AddPage();
-                $pdf->Cell(0, 5, $lang->translation('Primer aviso de cobro'), 0, 1);
-                $pdf->Ln(5);
-                if ($lang->translation('es') == 'es') {
-                    $fechaHoy = date('j') . ' de ' . $MES[date('n') - 1] . ' de ' . date('Y');
-                } else {
-                    $fechaHoy = $lang->translation($MES[date('n') - 1]) . ' ' . date('j') . ', ' . date('Y');
-                }
-                $pdf->Cell(0, 5, $fechaHoy, 0, 1);
-                $pdf->Ln(5);
-                $pdf->Cell(0, 5, $lang->translation('Padre, Madre o Encargado'), 0, 1);
-                $pdf->Ln(3);
-                $pdf->SetFont('Arial', 'B', 12);
-                $estu = DB::table('madre')
-                    ->whereRaw("id='$r->id'")->first();
-                $pdf->Cell(0, 5, $estu->madre ?? '', 0, 1);
-                $pdf->Cell(0, 5, $estu->padre ?? '', 0, 1);
-                $pdf->Ln(3);
-                $pdf->SetFont('Arial', '', 12);
-                $pdf->Cell(0, 5, $lang->translation('CUENTA') . ' # ' . $r->id, 0, 1);
-                $pdf->Ln(5);
-                $pdf->MultiCell(0, 6, $lang->translation('HEMOS REVISADO NUESTRAS CUENTAS A COBRAR Y ENCONTRAMOS QUE USTED A LA FECHA DE HOY ') . $fechaHoy . $lang->translation(' NO HA EFECTUADO EL PAGO CORRESPONDIENTE AL MES DE:'));
-                $pdf->Ln(5);
-                $pdf->SetFont('Arial', 'B', 12);
-                $TOTAL = 0;
-                foreach ($rs as $rd) {
-                    $deudas = 0;
-                    $pagos = 0;
-                    $p = DB::table('pagos')
-                        ->whereRaw("fecha_d='$rd->fecha_d' AND id='$r->id' AND year = '$year' AND fecha_d <= '$fecha' $fechaFinal and baja=''")->get();
-                    foreach ($p as $row) {
-                        $deudas += $row->deuda;
-                        $pagos += $row->pago;
-                    }
-                    $total = $deudas - $pagos;
-                    $TOTAL += $total;
-                    if ($total != 0) {
-                        $MES1 = strtoupper($MES[date('n', strtotime($rd->fecha_d)) - 1]);
-                        list($y, $m, $d) = explode("-", $rd->fecha_d);
-                        if ($m < 6) {
-                            $y4 = ' 20' . $y2;
-                        } else {
-                            $y4 = ' 20' . $y3;
-                        }
-
-                        if ($MES1 == $lang->translation('Junio') . ' ' . $y4) {
-                            $MES1 = $lang->translation('MATRICULA');
-                        }
-
-                        $pdf->Cell(37, 5, $lang->translation($MES1) . ' ' . $y4);
-                        $pdf->Cell(90, 5, '.......................................................................');
-                        $pdf->Cell(20, 5, number_format($total, 2), 0, 1, 'R');
-                    }
-                }
-                $pdf->Cell(37, 5, $lang->translation('BALANCE'));
-                $pdf->Cell(90, 5, '.......................................................................');
-                $pdf->Cell(20, 5, number_format($TOTAL, 2), 0, 1, 'R');
-                $pdf->SetFont('Arial', '', 11);
-                $pdf->Ln(5);
-                $pdf->Cell(0, 5, $lang->translation('NOTA IMPORTANTE:'), 0, 1);
-                $pdf->Ln(5);
-                $pdf->MultiCell(0, 7, $lang->translation('1. DESPUES DEL DIA 10 DE CADA MES SE COBRARAN $') . $chk . $lang->translation(' DE CARGOS POR DEMORA POR CUENTA.'));
-                $pdf->Ln(3);
-                $pdf->MultiCell(0, 7, $lang->translation('2. LOS PAGOS PUEDEN HACERSE MEDIANTE TARJETA DE CREDITO, ATH, ATHMOVIL BUSINESS, EFECTIVO, GIRO POSTAL.'));
-                $pdf->Ln(3);
-                $pdf->MultiCell(0, 7, $lang->translation('3. FAVOR DE HACER LOS ARREGLOS PERTINENTES PARA QUE LOS SERVICIOS EDUCATIVOS DE SU HIJO(A) NO SE VEAN AFECTADOS.'));
-                $pdf->Ln(3);
-
-                $file_name = "letter_" . $r->id . ".pdf";
-                $co_re = __RESEND_KEY_OTHER__;
-                $from = "{$colegio} <" . $co_re . ">";
-
-                //***************************************************
-                $mail = new Mail();
-                $title = $lang->translation('Primer aviso de cobro');
-                $subject = $lang->translation('Primer aviso de cobro');
-                $message = 'Cta. ' . $r->id;
-                $mail->Subject = $subject;
-                $emailsSent = 0;
-                $emailsError = 0;
-                $error = null;
-
-                $uploadHost = dirname($_SERVER['SCRIPT_URI']);
-                $target_dir = "attachments/";
-
-                if (!is_dir($target_dir)) {
-                    mkdir($target_dir);
-                }
-                $target_file = $file_name;
-                $files = [];
-
-                //$files[] = $uploadHost.'/'.$target_dir.$target_file;
-                //$files[] = ''.$target_dir.$target_file;
-                $files[] = $uploadHost . '/attachments/' . $file_name;
-
-                if (__RESEND == '1') {
-                    $file = $pdf->Output("attachments/" . $file_name, 'F');
-                }
-
-                if (__PHPMAIL == '1') {
-                    $file2 = $pdf->Output('', 'S');
-                    $mail->addStringAttachment($file2, $file_name);
-                }
-
-                $parents = DB::table('madre')->where('id', $r->id)->first();
-                $emails = [
-                    ['correo' => $parents->email_p, 'nombre' => $parents->padre],
-                    ['correo' => $parents->email_m, 'nombre' => $parents->madre]
-                ];
-                $to = [];
-                foreach ($emails as $email) {
-                    if ($email['correo'] !== '') {
-                        $mail->addAddress($email['correo'], $email['nombre']);
-                        $to[] = $email['correo'];
-                    }
-                }
-                //        $mail->addAddress("alf_med@hotmail.com", 'Alfredo Medina');
-                $message = "<center><h1>$title</h1></center><br/><br/><p>" . nl2br($title) . "</p>";
-
-                $mail->isHTML(true);
-                $mail->Body = "
-            <!DOCTYPE html>
-            <html lang='" . __LANG . "'>
-            <head>
-                <meta charset='UTF-8'>
-                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                <title>{$title}</title>
-            </head>
-            <body>
-            <br>
-            <p>{$message}</p>  
-            </body>
-            </html>
-            ";
-                //            <center><h2>{$title}</h2></center>
-
-
-
-                if (__PHPMAIL == '1') {
-                    $mail->send();
-                    $mail->ClearAddresses();
-                }
-                $mail->ClearAddresses();
-                $mail->ClearAttachments();
-                if (__RESEND == '1') {
-                    DB::table('email_queue')->insert([
-                        'from' => $from,
-                        'reply_to' => $reply_to,
-                        'to' => json_encode($to),
-                        'message' => $message,
-                        'text' => '',
-                        'subject' => $subject,
-                        'attachments' => json_encode($files),
-                        'user' => $user,
-                        'year' => $year,
-                        'id2' => $r->id,
-                        'fecha' => date('Y-m-d'),
-                        'nombre' => '',
-                    ]);
-                }
-
-
-//***************************************************
-
+if ($_POST['tipo'] === 'email') {
+    foreach ($files as $familyId => $file) {
+        $family = Family::find($familyId)->first();
+        $emails = [
+            ['correo' => $family->email_p, 'nombre' => $family->padre],
+            ['correo' => $family->email_m, 'nombre' => $family->madre]
+        ];
+        $to = [];
+        foreach ($emails as $email) {
+            if ($email['correo'] !== '') {
+                $to[] = $email['correo'];
             }
         }
+
+        Email::to('franklinomarflores@gmail.com')
+            ->subject(__('Primer aviso de cobro'))
+            ->body(__('Adjunto el primer aviso de cobro para la cuenta #') . $familyId)
+            ->attach($file, 'letter_' . $familyId . '.pdf')
+            ->queue();
     }
 }
