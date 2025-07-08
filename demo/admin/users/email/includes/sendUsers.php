522189f7680d7ab18df3c2667cc84897517143eb
Alfredo <alf_med@hotmail.com>
2025-07-08 17:13:16 -0400

    Sele cambio la forma de enviar los correos

--- demo/admin/users/email/includes/sendUsers.php
+++ demo/admin/users/email/includes/sendUsers.php
@@ -2,17 +2,40 @@
 require_once '../../../../app.php';
 
 use Classes\Lang;
-use Classes\Mail;
 use Classes\Util;
 use Classes\Route;
+use Classes\Mail;
 use Classes\Session;
 use Classes\DataBase\DB;
 use Classes\Controllers\Student;
 
+use App\Models\Admin;
+use App\Models\Family;
+
+use Classes\Email;
+use Illuminate\Support\Carbon;
+
+
 Session::is_logged();
+
+$today = Carbon::now();
+$files = [];
+
+$col = db::table('colegio')->whereRaw("usuario = 'administrador'")->first();
+$colegio = $col->colegio;
+
+$school = Admin::user(Session::id())->first();
+
+$year = $school->year2;
+$chk = $school->chk;
+$reply_to = $school->correo;
+$user = $school->usuario;
+
+
+
 $lang = new Lang([
     ['Usuario y contraseña', 'Username and password'],
-    ['Información de acceso', 'Access information'],
+    ['Información de acceso al área de padres.', "Parents' Area Access Information."],
     ['Esta es la información de acceso para los padres del estudiante', 'This is the access information for the parents of the student'],
     ['Usuario', 'Username'],
     ['Contraseña', 'Password'],
@@ -21,6 +44,9 @@ $lang = new Lang([
 ]);
 $students = $_POST['students'];
 $mail = new Mail();
+$co_re = __RESEND_KEY_OTHER__;
+$from = "{$colegio} <".$co_re.">";
+
 foreach ($students as $ss) {
     $student = new Student($ss);
    
@@ -30,25 +56,15 @@ foreach ($students as $ss) {
             ['correo' => $parents->email_m, 'nombre' => $parents->madre]
         ];
     
-    $emails = Util::toObject($emails);
-    $count = 0;
-    foreach ($emails as $email) {
-        if ($email->correo !== '') {
-            $count++;
-            $mail->addAddress($email->correo, $email->nombre);
-        }
-    }
-
     $link = Route::url('/foro/login.php', true);
+    $link = Route::url('', true);
     $schoolName = $student->info('colegio');
     $studentName = "{$student->id} {$student->nombre} {$student->apellidos}";
     $username = $parents->usuario;
     $password = $parents->clave;
     $messageTitle = $lang->translation('Usuario y contraseña');
-    $mail->isHTML(true);
-    $mail->Subject = $lang->translation('Información de acceso');
     $_lang = __LANG;
-   echo $mail->Body = "
+   $Body = "
    <!DOCTYPE html>
    <html lang='{$_lang}'>
    <head>
@@ -72,12 +88,35 @@ foreach ($students as $ss) {
    </body>
    </html>
    ";
-    if ($count > 0) {
-        if (!$mail->send()) {
-            echo "Error " . $mail->ErrorInfo;
+        $to = [];
+        foreach ($emails as $email) {
+            if ($email['correo'] !== '') {
+                $to[] = $email['correo'];
+            }
         }
-    }
-    Session::set('emailSent', $lang->translation("Se ha enviado el correo electrónico"));
-    $mail->ClearAddresses();
+
+
+//        Email::to($to)
+//            ->subject($messageTitle)
+//            ->body($Body)
+//            ->queue($to2);
+//            ->queue($student->id);
+
+
+        DB::table('email_queue')->insert([
+        'from' => utf8_encode($from),
+        'reply_to' => $reply_to,
+        'to' => json_encode($to),
+        'message' => utf8_encode($Body),
+        'text' => '',
+        'subject' => utf8_encode($messageTitle),
+        'user' => $user,
+        'year' => $year,
+        'id2' => $student->id,
+        'social_securities' => $student->ss,
+        ]);
+
+
+    
 }
 Route::redirect("/users/email/sendUsers.php");
