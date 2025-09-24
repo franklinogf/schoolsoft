2ae7bd69f725011f01853271ddf86e8148e36deb
Alfredo <alf_med@hotmail.com>
2025-09-24 09:44:01 -0400

    Codigos de Alias

---
+++ demo/admin/users/alias/_vti_cnf/codes.php
@@ -0,0 +1,19 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Sep 2025 13:34:10 -0000
+vti_author:SR|SchoolSoft\\Alfreddo
+vti_modifiedby:SR|SchoolSoft\\Alfreddo
+vti_nexttolasttimemodified:TW|24 Sep 2025 13:22:25 -0000
+vti_timecreated:TR|24 Sep 2025 12:55:05 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_syncofs_194.164.66.182\:21:TW|24 Sep 2025 13:22:30 -0000
+vti_syncwith_194.164.66.182\:21:TW|24 Sep 2025 13:22:25 -0000
+vti_cacheddtm:TX|24 Sep 2025 13:22:25 -0000
+vti_filesize:IR|5306
+vti_cachedbodystyle:SR|<body>
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
