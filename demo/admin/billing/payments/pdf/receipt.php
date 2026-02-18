b8f7357cbd37eb5c1db5b7eee9dd7428f0713708
Alfredo Medina <alf_med@hotmail.com>
2026-02-18 10:12:12 -0500

    Se añadio tipo de pago

--- demo/admin/billing/payments/pdf/receipt.php
+++ demo/admin/billing/payments/pdf/receipt.php
@@ -30,7 +30,7 @@ $paymentTypes = [
     '16' => 'Acuden-Vales',
     '17' => 'VA Prog',
     '18' => 'Colegio',
-    '19' => 'Bd! Pago APP',
+    '19' => 'Pago APP',
 ];
 
 $months = __LANG === 'es' ?
