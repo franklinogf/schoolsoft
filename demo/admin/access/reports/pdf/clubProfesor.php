1b1cc716077ae1dec71f70dd24b32a84ee4775e1
Alfredo Medina <alf_med@hotmail.com>
2026-03-04 10:06:26 -0500

    Se arreglo PDF

--- demo/admin/access/reports/pdf/clubProfesor.php
+++ demo/admin/access/reports/pdf/clubProfesor.php
@@ -26,17 +26,18 @@ $pdf->SetTitle($lang->translation("Lista de clud Profesores") . " $year", true);
 $pdf->Fill();
 
 $pdf->AddPage('L');
-$pdf->SetFont('Arial', 'B', 15);
+$pdf->SetFont('Arial', 'B', 14);
 $pdf->Cell(0, 5, $lang->translation("Lista de club Profesores") . " $year", 0, 1, 'C');
 
 $pdf->Ln(5);
 $pdf->SetFont('Arial', 'B', 10);
 $pdf->Cell(7, 5, '', 1, 0, 'C', true);
-$pdf->Cell(15, 5, $lang->translation("ID"), 1, 0, 'C', true);
-$pdf->Cell(70, 5, $lang->translation("Profesor"), 1, 0, 'C', true);
-$pdf->Cell(60, 5, $lang->translation("Club"), 1, 0, 'C', true);
-$pdf->Cell(60, 5, $lang->translation("Presidente"), 1, 0, 'C', true);
-$pdf->Cell(60, 5, $lang->translation("Vice-Presidente"), 1, 1, 'C', true);
+//$pdf->Cell(15, 5, $lang->translation("ID"), 1, 0, 'C', true);
+$pdf->Cell(55, 5, $lang->translation("Profesor"), 1, 0, 'C', true);
+$pdf->Cell(55, 5, $lang->translation("Club"), 1, 0, 'C', true);
+$pdf->Cell(55, 5, $lang->translation("Presidente"), 1, 0, 'C', true);
+$pdf->Cell(55, 5, $lang->translation("Vice-Presidente"), 1, 0, 'C', true);
+$pdf->Cell(55, 5, $lang->translation("Secretario(a)"), 1, 1, 'C', true);
 $pdf->ln(2);
 $pdf->SetFont('Arial', '', 10);
 
@@ -47,43 +48,43 @@ $teachers = DB::table('profesor')->where([
 ])->orderBy('apellidos')->get();
 foreach ($teachers as $count => $teacher) {
     $pdf->Cell(7, 5, $count + 1, 0, 0, 'C');
-    $pdf->Cell(15, 5, $teacher->id, 0, 0, 'C');
-   $pdf->Cell(70, 5, $teacher->apellidos . ' ' . $teacher->nombre);
-   $pdf->Cell(60, 5, $teacher->club1);
-   $pdf->Cell(60, 5, $teacher->pre1);
-   $pdf->Cell(60, 5, $teacher->vi1);
-   $pdf->Cell(60, 5, $teacher->se1, 0, 1);
+//    $pdf->Cell(15, 5, $teacher->id, 0, 0, 'C');
+   $pdf->Cell(55, 5, $teacher->apellidos . ' ' . $teacher->nombre);
+   $pdf->Cell(55, 5, $teacher->club1);
+   $pdf->Cell(55, 5, $teacher->pre1);
+   $pdf->Cell(55, 5, $teacher->vi1);
+   $pdf->Cell(55, 5, $teacher->se1, 0, 1);
     if ($teacher->club2 !='')
        {
-       $pdf->Cell(92, 5, '', 0, 0, 'C');
-      $pdf->Cell(60, 5, $teacher->club2);
-      $pdf->Cell(60, 5, $teacher->pre2);
-      $pdf->Cell(60, 5, $teacher->vi2);
-      $pdf->Cell(60, 5, $teacher->se2, 0, 1);
+       $pdf->Cell(62, 5, '', 0, 0, 'C');
+      $pdf->Cell(55, 5, $teacher->club2);
+      $pdf->Cell(55, 5, $teacher->pre2);
+      $pdf->Cell(55, 5, $teacher->vi2);
+      $pdf->Cell(55, 5, $teacher->se2, 0, 1);
        }
     if ($teacher->club3 !='')
        {
-       $pdf->Cell(92, 5, '', 0, 0, 'C');
-      $pdf->Cell(60, 5, $teacher->club3);
-      $pdf->Cell(60, 5, $teacher->pre3);
-      $pdf->Cell(60, 5, $teacher->vi3);
-      $pdf->Cell(60, 5, $teacher->se3, 0, 1);
+       $pdf->Cell(62, 5, '', 0, 0, 'C');
+      $pdf->Cell(55, 5, $teacher->club3);
+      $pdf->Cell(55, 5, $teacher->pre3);
+      $pdf->Cell(55, 5, $teacher->vi3);
+      $pdf->Cell(55, 5, $teacher->se3, 0, 1);
        }
     if ($teacher->club4 !='')
        {
-       $pdf->Cell(92, 5, '', 0, 0, 'C');
-      $pdf->Cell(60, 5, $teacher->club4);
-      $pdf->Cell(60, 5, $teacher->pre4);
-      $pdf->Cell(60, 5, $teacher->vi4);
-      $pdf->Cell(60, 5, $teacher->se4, 0, 1);
+       $pdf->Cell(62, 5, '', 0, 0, 'C');
+      $pdf->Cell(55, 5, $teacher->club4);
+      $pdf->Cell(55, 5, $teacher->pre4);
+      $pdf->Cell(55, 5, $teacher->vi4);
+      $pdf->Cell(55, 5, $teacher->se4, 0, 1);
        }
     if ($teacher->club5 !='')
        {
-       $pdf->Cell(92, 5, '', 0, 0, 'C');
-      $pdf->Cell(60, 5, $teacher->club5);
-      $pdf->Cell(60, 5, $teacher->pre5);
-      $pdf->Cell(60, 5, $teacher->vi5);
-      $pdf->Cell(60, 5, $teacher->se5, 0, 1);
+       $pdf->Cell(62, 5, '', 0, 0, 'C');
+      $pdf->Cell(55, 5, $teacher->club5);
+      $pdf->Cell(55, 5, $teacher->pre5);
+      $pdf->Cell(55, 5, $teacher->vi5);
+      $pdf->Cell(55, 5, $teacher->se5, 0, 1);
    }
 }
 
