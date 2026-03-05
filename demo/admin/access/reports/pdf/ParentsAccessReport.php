ebd0798695e2c5bdaca875bc7410d9253df762f5
Alfredo Medina <alf_med@hotmail.com>
2026-03-05 11:14:15 -0500

    Informe de acceso a los padres PDF

--- demo/admin/access/reports/pdf/ParentsAccessReport.php
+++ demo/admin/access/reports/pdf/ParentsAccessReport.php
@@ -30,7 +30,7 @@ $year = $school->year('year2');
 
 $from = $_POST['from'];
 $to = $_POST['to'];
-$option = $_POST['option'];
+$option = $_POST['option'] ?? '';
 
 $pdf = new PDF();
 $pdf->SetTitle($lang->translation("Informe de acceso de los padres") . " $year", true);
@@ -73,12 +73,13 @@ if ($option === 'student') {
 } else {
     $school = new School(Session::id());
     $year = $school->year('year2');
-    $grade = $_POST['grade'];
+    $grade = $_POST['student'];
     $type = $_POST['type'] ?? 'list';
     $grades = $grade !== '' ? [$grade] : $school->allGrades();
         foreach ($grades as $grade) {
         $students = DB::table('year')->where([
             ['grado', $grade],
+            ['codigobaja', 0],
             ['year', $year],
         ])->orderBy("grado, apellidos, nombre")->get();
         
@@ -89,7 +90,7 @@ if ($option === 'student') {
                 $pdf->SetFont('Arial', 'B', 15);
                 $pdf->Cell(0, 5, $lang->translation("Informe de acceso de los padres") . " $year", 0, 1, 'C');
                 $pdf->SetFont('Arial', '', 12);
-                $pdf->Ln(10);
+                $pdf->Ln(5);
                 $pdf->SetFont('Arial', 'B', 12);
                 $pdf->Cell(0, 5, $lang->translation("Desde") . ": $from / " . $lang->translation("Hasta") . ": $to", 0, 1, 'C');
                 $pdf->Ln(5);
@@ -98,14 +99,13 @@ if ($option === 'student') {
                 $pdf->Cell(135, 5, $stud->apellidos.' '.$stud->nombre, 0, 0, 'L');
                 $pdf->Cell(20, 5, $lang->translation("Grado"), 1, 0, 'C', true);
                 $pdf->Cell(20, 5, $stud->grado, 0, 1, 'L');
-                $pdf->Ln(5);
 
                 $pdf->Cell(20, 5, '', 1, 0, 'C', true);
-                $pdf->Cell(20, 5, 'ID', 1, 0, 'C', true);
-                $pdf->Cell(50, 5, $lang->translation("Fecha"), 1, 0, 'C', true);
-                $pdf->Cell(50, 5, $lang->translation("Hora"), 1, 0, 'C', true);
-                $pdf->Cell(50, 5, $lang->translation("IP"), 1, 1, 'C', true);
-                $pdf->SetFont('Arial', '', 12);
+//                $pdf->Cell(20, 5, 'ID', 1, 0, 'C', true);
+                $pdf->Cell(30, 5, $lang->translation("Fecha"), 1, 0, 'C', true);
+                $pdf->Cell(30, 5, $lang->translation("Hora"), 1, 0, 'C', true);
+                $pdf->Cell(110, 5, $lang->translation("IP"), 1, 1, 'C', true);
+                $pdf->SetFont('Arial', '', 10);
                 $attendances = DB::table('entradas')->where([
                   ['id', $stud->id],
                   ['fecha', '>=', $from],
@@ -114,10 +114,10 @@ if ($option === 'student') {
                foreach ($attendances as $attendance) 
                        {
                        $pdf->Cell(20, 5, $count, 1, 0, 'C');
-                       $pdf->Cell(20, 5, $attendance->id, 1);
-                       $pdf->Cell(50, 5, $attendance->fecha, 1);
-                       $pdf->Cell(50, 5, $attendance->hora, 1, 0, 'C');
-                       $pdf->Cell(50, 5, $attendance->ip, 1, 1, 'C');
+//                       $pdf->Cell(20, 5, $attendance->id, 1);
+                       $pdf->Cell(30, 5, $attendance->fecha, 1, 0, 'C');
+                       $pdf->Cell(30, 5, $attendance->hora, 1, 0, 'C');
+                       $pdf->Cell(110, 5, $attendance->ip, 1, 1, 'C');
                        $count++;
                        }
                }
