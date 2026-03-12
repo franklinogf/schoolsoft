01cfe49d741ac47a6455f82ccfd24a2897647bc6
Franklin Gonzalez <franklinomarflores@gmail.com>
2026-03-12 15:21:56 -0400

    Refactor catalog.php to improve course handling and teacher retrieval logic

--- demo/admin/access/catalog.php
+++ demo/admin/access/catalog.php
@@ -1,12 +1,14 @@
 <?php
 require_once __DIR__ . '/../../app.php';
 
+use App\Models\Subject;
+use App\Models\Teacher;
 use Classes\Lang;
 use Classes\Route;
 use Classes\Session;
 use Classes\DataBase\DB;
 use Classes\Controllers\School;
-use Classes\Controllers\Teacher;
+
 
 Session::is_logged();
 $lang = new Lang([
@@ -38,19 +40,14 @@ $years = DB::table('year')->select("DISTINCT year")->get();
 $school = new School(Session::id());
 
 
-$teachers = new Teacher;
-//$teachers = $teachers->all();
-
-$teachers = DB::table('profesor')->where([
-    ['activo', 'Activo']
-])->orderBy('apellidos')->get();
 
 
 if (isset($_REQUEST['search'])) {
-    $thisCourse = DB::table('cursos')->where('mt', $_REQUEST['course'])->first();
+    $thisCourse = Subject::query()->find($_REQUEST['course']);
 }
+
 if (isset($_REQUEST['save'])) {
-    $teacher = new Teacher($_POST['teacherId']);
+    $teacher = Teacher::find($_POST['teacherId']);
     $thisCourse = DB::table('cursos')->where('mt', $_REQUEST['courseId'])->update([
         'maestro' => "$teacher->nombre $teacher->apellidos",
         'curso' => $_POST['curso'],
@@ -67,7 +64,7 @@ if (isset($_REQUEST['save'])) {
         'verano' => $_POST['verano'] ?? '',
     ]);
 
-    $thisCourse2 = DB::table('padres')->where([
+    DB::table('padres')->where([
         ['curso', $_POST['curso']],
         ['year', $school->info('year2')]
     ])->update([
@@ -83,7 +80,7 @@ if (isset($_REQUEST['save'])) {
         'verano' => $_POST['verano'] ?? '',
     ]);
     for ($a = 2; $a <= 6; $a++) {
-        $thisCourse2 = DB::table("padres$a")->where([
+        DB::table("padres$a")->where([
             ['curso', $_POST['curso']],
             ['year', $school->info('year2')]
         ])->update([
@@ -114,9 +111,9 @@ if (isset($_REQUEST['create'])) {
 if (isset($_REQUEST['delete'])) {
     DB::table('cursos')->where('mt', $_REQUEST['courseId'])->delete();
 }
-$courses = DB::table('cursos')->where([
-    ['year', $school->info('year2')]
-])->orderBy('curso')->get();
+$courses = Subject::all();
+
+$teachers = Teacher::query()->where('activo', 'Activo')->orderBy('apellidos')->get();
 ?>
 <!DOCTYPE html>
 <html lang="<?= __LANG ?>">
@@ -151,98 +148,100 @@ $courses = DB::table('cursos')->where([
                     <input name="search" class="btn btn-primary mx-auto d-block mt-1" type="submit" value="<?= $lang->translation("Buscar") ?>">
                 </form>
 
-                <form class="mt-3" method="POST">
-                    <?php if (isset($_POST['search'])) : ?>
-                        <input type="hidden" name="courseId" value="<?= $_REQUEST['course'] ?>">
-                    <?php endif ?>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="form-group col-6 px-0">
-                                <label for="curso"><?= $lang->translation("Curso") ?></label>
-                                <input type="text" value='<?= $thisCourse->curso ?? '' ?>' class="form-control" name='curso' id="curso" required>
+                <?php if (isset($thisCourse)) : ?>
+                    <form class="mt-3" method="POST">
+                        <?php if (isset($_POST['search'])) : ?>
+                            <input type="hidden" name="courseId" value="<?= $_REQUEST['course'] ?>">
+                        <?php endif ?>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group col-6 px-0">
+                                    <label for="curso"><?= $lang->translation("Curso") ?></label>
+                                    <input type="text" value='<?= $thisCourse->curso ?? '' ?>' class="form-control" name='curso' id="curso" required>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="desc1"><?= $lang->translation("Descripción") . ' ' . $lang->translation("español") ?></label>
+                                    <input type="text" value='<?= $thisCourse->desc1 ?? '' ?>' class="form-control" maxlength="40" name='desc1' id="desc1" required>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="desc2"><?= $lang->translation("Descripción") . ' ' . $lang->translation("inglés") ?></label>
+                                    <input type="text" value='<?= $thisCourse->desc2 ?? '' ?>' class="form-control" maxlength="40" name='desc2' id="desc2" required>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="credito"><?= $lang->translation("Crédito") ?></label>
+                                    <input type="text" value='<?= $thisCourse->credito ?? '' ?>' class="form-control --float" name='credito' id="credito" required>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="peso"><?= $lang->translation("Peso") ?></label>
+                                    <input type="text" value='<?= $thisCourse->peso ?? '' ?>' class="form-control --float" name='peso' id="peso" required>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="teacherId"><?= $lang->translation("Maestro") ?></label>
+                                    <select class="form-control selectpicker w-100" name="teacherId" id="teacherId" data-live-search="true" required>
+                                        <option value=""><?= $lang->translation("Seleccionar") ?></option>
+                                        <?php foreach ($teachers as $teacher) : ?>
+                                            <option <?= $teacher->id == $thisCourse->id ?? '' ? 'selected=""' : '' ?> value="<?= $teacher->id ?>"><?= "$teacher->apellidos $teacher->nombre - $teacher->id" ?></option>
+                                        <?php endforeach ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="entrada"><?= $lang->translation("Horario entrada") ?></label>
+                                    <input type="text" value='<?= $thisCourse->entrada ?? '' ?>' class="form-control" maxlength="7" name='entrada' id="entrada">
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="salida"><?= $lang->translation("Horario salida") ?></label>
+                                    <input type="text" value='<?= $thisCourse->salida ?? '' ?>' class="form-control" maxlength="7" name='salida' id="salida">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group col-6 px-0">
+                                    <label for="dias"><?= $lang->translation("Días") ?></label>
+                                    <input type="text" value='<?= $thisCourse->dias ?? '' ?>' class="form-control" maxlength="6" name='dias' id="dias">
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <div class="form-group">
+                                    <label for="ava"><?= $lang->translation("Avanzada") ?></label>
+                                    <select class="form-control" name="ava" id="ava" required>
+                                        <option <?= $thisCourse->ava ?? '' == 'No' ? 'selected=""' : '' ?> value="No">No</option>
+                                        <option <?= $thisCourse->ava ?? '' == 'Si' ? 'selected=""' : '' ?> value="Si"><?= $lang->translation("Si") ?></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-7">
+                                <label for="valor"><?= $lang->translation("Valor") ?></label>
+                                <div class="input-group">
+                                    <input type="text" value='<?= $thisCourse->valor ?? '' ?>' class="form-control --float" name='valor' id="valor" disabled>
+                                    <select class="form-control" name="verano" id="verano" disabled>
+                                        <option <?= $thisCourse->verano ?? '' == '' ? 'selected=""' : '' ?> value=""><?= $lang->translation("Regular") ?></option>
+                                        <option <?= $thisCourse->verano ?? '' == '2' ? 'selected=""' : '' ?> value="2"><?= $lang->translation("Verano") ?></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 text-center">
+                                <button type="submit" class="btn btn-primary" name="<?= isset($_POST['search']) ? 'save' : 'create' ?>" type="submit"><?= $lang->translation(isset($_POST['search']) ? 'Guardar' : 'Crear') ?></button>
+                                <?php if (isset($_POST['search'])) : ?>
+                                    <a href="catalog.php" class="btn btn-secondary"><?= $lang->translation('Limpiar') ?></a>
+                                    <input class="btn btn-danger" name="delete" style="width: 90px;" type="submit" formnovalidate value="<?= $lang->translation('Eliminar') ?>" onclick="return confirmar('<?= $lang->translation('Estás seguro que quieres borrar el curso?') ?>')" />
+                                <?php endif ?>
                             </div>
                         </div>
-                        <div class="col-12">
-                            <div class="form-group">
-                                <label for="desc1"><?= $lang->translation("Descripción") . ' ' . $lang->translation("español") ?></label>
-                                <input type="text" value='<?= $thisCourse->desc1 ?? '' ?>' class="form-control" maxlength="40" name='desc1' id="desc1" required>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="form-group">
-                                <label for="desc2"><?= $lang->translation("Descripción") . ' ' . $lang->translation("inglés") ?></label>
-                                <input type="text" value='<?= $thisCourse->desc2 ?? '' ?>' class="form-control" maxlength="40" name='desc2' id="desc2" required>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group">
-                                <label for="credito"><?= $lang->translation("Crédito") ?></label>
-                                <input type="text" value='<?= $thisCourse->credito ?? '' ?>' class="form-control --float" name='credito' id="credito" required>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group">
-                                <label for="peso"><?= $lang->translation("Peso") ?></label>
-                                <input type="text" value='<?= $thisCourse->peso ?? '' ?>' class="form-control --float" name='peso' id="peso" required>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="form-group">
-                                <label for="teacherId"><?= $lang->translation("Maestro") ?></label>
-                                <select class="form-control selectpicker w-100" name="teacherId" id="teacherId" data-live-search="true" required>
-                                    <option value=""><?= $lang->translation("Seleccionar") ?></option>
-                                    <?php foreach ($teachers as $teacher) : ?>
-                                        <option <?= $teacher->id == $thisCourse->id ?? '' ? 'selected=""' : '' ?> value="<?= $teacher->id ?>"><?= "$teacher->apellidos $teacher->nombre - $teacher->id" ?></option>
-                                    <?php endforeach ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group">
-                                <label for="entrada"><?= $lang->translation("Horario entrada") ?></label>
-                                <input type="text" value='<?= $thisCourse->entrada ?? '' ?>' class="form-control" maxlength="7" name='entrada' id="entrada">
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group">
-                                <label for="salida"><?= $lang->translation("Horario salida") ?></label>
-                                <input type="text" value='<?= $thisCourse->salida ?? '' ?>' class="form-control" maxlength="7" name='salida' id="salida">
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="form-group col-6 px-0">
-                                <label for="dias"><?= $lang->translation("Días") ?></label>
-                                <input type="text" value='<?= $thisCourse->dias ?? '' ?>' class="form-control" maxlength="6" name='dias' id="dias">
-                            </div>
-                        </div>
-                        <div class="col-5">
-                            <div class="form-group">
-                                <label for="ava"><?= $lang->translation("Avanzada") ?></label>
-                                <select class="form-control" name="ava" id="ava" required>
-                                    <option <?= $thisCourse->ava ?? '' == 'No' ? 'selected=""' : '' ?> value="No">No</option>
-                                    <option <?= $thisCourse->ava ?? '' == 'Si' ? 'selected=""' : '' ?> value="Si"><?= $lang->translation("Si") ?></option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-7">
-                            <label for="valor"><?= $lang->translation("Valor") ?></label>
-                            <div class="input-group">
-                                <input type="text" value='<?= $thisCourse->valor ?? '' ?>' class="form-control --float" name='valor' id="valor" disabled>
-                                <select class="form-control" name="verano" id="verano" disabled>
-                                    <option <?= $thisCourse->verano ?? '' == '' ? 'selected=""' : '' ?> value=""><?= $lang->translation("Regular") ?></option>
-                                    <option <?= $thisCourse->verano ?? '' == '2' ? 'selected=""' : '' ?> value="2"><?= $lang->translation("Verano") ?></option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-12 text-center">
-                            <button type="submit" class="btn btn-primary" name="<?= isset($_POST['search']) ? 'save' : 'create' ?>" type="submit"><?= $lang->translation(isset($_POST['search']) ? 'Guardar' : 'Crear') ?></button>
-                            <?php if (isset($_POST['search'])) : ?>
-                                <a href="catalog.php" class="btn btn-secondary"><?= $lang->translation('Limpiar') ?></a>
-                                <input class="btn btn-danger" name="delete" style="width: 90px;" type="submit" formnovalidate value="<?= $lang->translation('Eliminar') ?>" onclick="return confirmar('<?= $lang->translation('Estás seguro que quieres borrar el curso?') ?>')" />
-                            <?php endif ?>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                <?php endif ?>
             </div>
         </div>
 
