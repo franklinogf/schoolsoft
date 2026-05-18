b1901a8db9956e32d5b8449d528a64be9adaa0f7
Alfredo Medina <alf_med@hotmail.com>
2026-05-18 14:49:56 -0400

    sSe añadieron los trimestres

--- demo/admin/access/gradesReports/pdf/ListaPromedios.php
+++ demo/admin/access/gradesReports/pdf/ListaPromedios.php
@@ -14,7 +14,7 @@ $lang = new Lang([
     ['Lista de promedios', 'List of averages'],
     ["Profesor", "Teacher:"],
     ["Grado:", "Grade:"],
-    ["Descripción", "Description"],
+    ["Descripci&#65533;n", "Description"],
     ['Apellidos', 'Lasname'],
     ['Nombre', 'Name'],
     ['Grado', 'Grade'],
@@ -96,6 +96,7 @@ else
         ])->orderBy('grado')->first();
    }
 
+
 foreach ($allGrades as $grade) {
         $studens = DB::table('year')->where([
           ['year', $year],
@@ -111,54 +112,112 @@ foreach ($allGrades as $grade) {
                  ['curso', 'NOT LIKE', '%AA-%']
                ])->orderBy('orden')->get();
                $a=0;$t=0;
+               $a1=0;$t1=0;
                $a2=0;$t2=0;
                $a3=0;$t3=0;
+               $a4=0;$t4=0;
+               $a5=0;$t5=0;
+               $a6=0;$t6=0;
+               $a7=0;$t7=0;
                foreach ($cursos as $curso) 
                        {
-                       if ($suma == 'N' and is_numeric($curso->final))
+
+                       if ($suma == 'N' and $curso->nota1 > 0)
                           {
-                          $a=$a+$curso->final;$t=$t+1;
+                          $a1=$a1+$curso->nota1;$t1=$t1+1;
                           }
-                       if ($suma == 'N' and is_numeric($curso->sem1))
+                       if ($suma == 'N' and $curso->nota2 > 0)
                           {
-                          $a2=$a2+$curso->sem1;$t2=$t2+1;
+                          $a2=$a2+$curso->nota2;$t2=$t2+1;
                           }
-                       if ($suma == 'N' and is_numeric($curso->sem2))
+                       if ($suma == 'N' and $curso->nota3 > 0)
                           {
-                          $a3=$a3+$curso->sem2;$t3=$t3+1;
+                          $a3=$a3+$curso->nota3;$t3=$t3+1;
                           }
-                       if ($suma == 'C' and is_numeric($curso->final) and $curso->credito > 0)
+                       if ($suma == 'N' and $curso->nota4 > 0)
                           {
-                          $a=$a+round($curso->final*$curso->credito,0);$t=$t+$curso->credito;
+                          $a4=$a4+$curso->nota4;$t4=$t4+1;
                           }
-                       if ($suma == 'C' and is_numeric($curso->sem1) and $curso->credito > 0)
+
+                       if ($suma == 'N' and $curso->final > 0)
                           {
-                          $a2=$a2+round($curso->sem1*$curso->credito,0);$t2=$t2+$curso->credito;
+                          $a7=$a7+$curso->final;$t7=$t7+1;
                           }
-                       if ($suma == 'C' and is_numeric($curso->sem2) and $curso->credito > 0)
+                       if ($suma == 'N' and $curso->sem1 > 0)
                           {
-                          $a3=$a3+round($curso->sem2*$curso->credito,0);$t3=$t3+$curso->credito;
+                          $a5=$a5+$curso->sem1;$t5=$t5+1;
+                          }
+                       if ($suma == 'N' and $curso->sem2 > 0)
+                          {
+                          $a6=$a6+$curso->sem2;$t6=$t6+1;
+                          }
+
+                       if ($suma == 'C' and $curso->nota1 > 0 and $curso->credito > 0)
+                          {
+                          $a1=$a1+round($curso->nota1*$curso->credito,0);$t1=$t1+$curso->credito;
+                          }
+                       if ($suma == 'C' and $curso->nota2 > 0 and $curso->credito > 0)
+                          {
+                          $a2=$a2+round($curso->nota2*$curso->credito,0);$t2=$t2+$curso->credito;
+                          }
+                       if ($suma == 'C' and $curso->nota3 > 0 and $curso->credito > 0)
+                          {
+                          $a3=$a3+round($curso->nota3*$curso->credito,0);$t3=$t3+$curso->credito;
+                          }
+                       if ($suma == 'C' and $curso->nota4 > 0 and $curso->credito > 0)
+                          {
+                          $a4=$a4+round($curso->nota4*$curso->credito,0);$t4=$t4+$curso->credito;
+                          }
+
+
+
+                       if ($suma == 'C' and $curso->final > 0 and $curso->credito > 0)
+                          {
+                          $a7=$a7+round($curso->final*$curso->credito,0);$t7=$t7+$curso->credito;
+                          }
+                       if ($suma == 'C' and $curso->sem1 > 0 and $curso->credito > 0)
+                          {
+                          $a5=$a5+round($curso->sem1*$curso->credito,0);$t5=$t5+$curso->credito;
+                          }
+                       if ($suma == 'C' and $curso->sem2 > 0 and $curso->credito > 0)
+                          {
+                          $a6=$a6+round($curso->sem2*$curso->credito,0);$t6=$t6+$curso->credito;
                           }
                        }
-               if ($t > 0)
+               if ($t7 > 0)
                   {
-                  $b=round($a/$t,$r);
+                  $b7=round($a7/$t7,$r);
+                  $b1=0;
                   $b2=0;
                   $b3=0;
+                  $b4=0;
+                  $b5=0;
+                  $b6=0;
+               if ($t1 > 0){$b1=round($a1/$t1,$r);}
                if ($t2 > 0){$b2=round($a2/$t2,$r);}
                if ($t3 > 0){$b3=round($a3/$t3,$r);}
-                  $updates = ['fin' => $b,
-                             'se1' => $b2,
-                             'se2' => $b3,];
+               if ($t4 > 0){$b4=round($a4/$t4,$r);}
+               if ($t5 > 0){$b5=round($a5/$t5,$r);}
+               if ($t6 > 0){$b6=round($a6/$t6,$r);}
+                  $updates = [
+                             'tr1' => $b1,
+                             'tr2' => $b2,
+                             'tr3' => $b3,
+                             'tr4' => $b4,
+                             'fin' => $b7,
+                             'se1' => $b5,
+                             'se2' => $b6,];
                   DB::table('year')->where('mt', $studen->mt)->update($updates);
                   }
                }
                
         }
 
+
+//$pdf = new nPDF();
 $pdf = new PDF();
 $cur = $_POST['curso'] ?? '';
-$cl = $_POST['cl'] ?? '';
+$cl = $_POST['cl'];
 $x = 0;
 if ($grado == 'all')
    {
@@ -187,26 +246,35 @@ foreach ($allGrades as $grade)
       $pdf->SetFont('Arial', 'B', 12);
       $pdf->Cell(0, 5, $lang->translation("Lista de promedios").' / '.$grade." / $tt / $year", 0, 1, 'C');
       $pdf->Ln(5);
-      $pdf->SetFont('Arial', '', 12);
+      $pdf->SetFont('Arial', '', 11);
       $pdf->Fill();
-      $pdf->Cell(10, 5, '', 1, 0, 'C', true);
+      $pdf->Cell(7, 5, '', 1, 0, 'C', true);
       $pdf->Cell(55, 5, $lang->translation("Apellidos"), 1, 0, 'C', true);
       $pdf->Cell(45, 5, $lang->translation("Nombre"), 1, 0, 'C', true);
-      $pdf->Cell(25, 5, 'S-1', 1, 0, 'C', true);
-      $pdf->Cell(25, 5, 'S-2', 1, 0, 'C', true);
-      $pdf->Cell(25, 5, 'Final', 1, 1, 'C', true);
-      $pdf->SetFont('Arial', '', 11);
+      $pdf->Cell(12, 5, 'T-1', 1, 0, 'C', true);
+      $pdf->Cell(12, 5, 'T-2', 1, 0, 'C', true);
+      $pdf->Cell(12, 5, 'S-1', 1, 0, 'C', true);
+      $pdf->Cell(12, 5, 'S-2', 1, 0, 'C', true);
+      $pdf->Cell(12, 5, 'T-3', 1, 0, 'C', true);
+      $pdf->Cell(12, 5, 'T-4', 1, 0, 'C', true);
+      $pdf->Cell(12, 5, 'Final', 1, 1, 'C', true);
+      $pdf->SetFont('Arial', '', 10);
       $n = 0;
       foreach ($students as $estu) 
               {
               $n = $n +1;
-              $pdf->Cell(10, 5, $n, 1, 0, 'R');
+              $pdf->Cell(7, 5, $n, 1, 0, 'R');
               $pdf->Cell(55, 5, $estu->apellidos, $cl, 0, 'L');
               $pdf->Cell(45, 5, $estu->nombre, $cl, 0, 'L');
-              $pdf->Cell(25, 5, number_format($estu->se1,$r), $cl, 0, 'R');
-              $pdf->Cell(25, 5, number_format($estu->se2,$r), $cl, 0, 'R');
-              $pdf->Cell(25, 5, number_format($estu->fin,$r), $cl, 1, 'R');
+              $pdf->Cell(12, 5, number_format($estu->tr1,$r), $cl, 0, 'R');
+              $pdf->Cell(12, 5, number_format($estu->tr2,$r), $cl, 0, 'R');
+              $pdf->Cell(12, 5, number_format($estu->se1,$r), $cl, 0, 'R');
+              $pdf->Cell(12, 5, number_format($estu->tr3,$r), $cl, 0, 'R');
+              $pdf->Cell(12, 5, number_format($estu->tr4,$r), $cl, 0, 'R');
+              $pdf->Cell(12, 5, number_format($estu->se2,$r), $cl, 0, 'R');
+              $pdf->Cell(12, 5, number_format($estu->fin,$r), $cl, 1, 'R');
               }
       }
+
 
 $pdf->Output();
