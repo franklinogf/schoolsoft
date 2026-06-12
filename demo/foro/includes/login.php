de2516415a2cc7ac98ed7b0457c8b6a1fbd27392
Franklin Gonzalez <franklinomarflores@gmail.com>
2026-06-12 12:11:44 -0400

    Se mejoró el proceso de inicio de sesión para incluir validación de usuarios docentes y estudiantes.

--- demo/foro/includes/login.php
+++ demo/foro/includes/login.php
@@ -1,15 +1,56 @@
 <?php
 
-
-use Classes\Login;
+use App\Models\Student;
+use App\Models\Teacher;
 use Classes\Route;
+
 include '../../app.php';
 
-if($_SERVER["REQUEST_METHOD"] == 'POST'){
-   
-   Login::login($_POST,'foro');
 
-}else{
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+
+   $location = null;
+
+   $teacherUser =  Teacher::where([
+      ['usuario', $_POST['username']],
+      ['clave', $_POST['password']]
+   ])->first();
+
+
+   if ($teacherUser) {
+      $location = 'foro/profesor/';
+      $user = $teacherUser;
+   } else {
+
+      $studentUser =  Student::where([
+         ['usuario', $_POST['username']],
+         ['clave', $_POST['password']]
+      ])->first();
+
+      if ($studentUser) {
+         $location = 'foro/estudiante/';
+         $user = $studentUser;
+      }
+   }
+
+
+   if ($user && $location) {
+
+      $_SESSION['logged'] = [
+         'acronym' => __SCHOOL_ACRONYM,
+         'location' => 'foro',
+         "user" => ['id' => $user->id],
+      ];
+      $_SESSION['start'] = time();
+      $_SESSION['id1'] = $user->id;
+      $_SESSION['usua1'] = $user->usuario;
+
+      Route::redirect($location, false);
+   } else {
+      $_SESSION['errorLogin'] = __("No coincide con los datos, intentelo otra vez");
+
+      Route::redirect();
+   }
+} else {
    Route::error();
 }
- 
